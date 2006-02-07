79c498590fdca3047fdf6440225c1b0d5089a37f
kalli <kalli@55547f66-8dd2-3569-90c8-08ced114d07d>
2006-02-07 19:17:34 +0000

    EWE Version 1.49

--- compile.bat
+++ compile.bat
@@ -1,1 +1,1 @@
-javac -cp lib/CompileEwe.zip;./lib/ewesoft.zip;./lib/EwesoftRegex.zip;./lib/HTML.zip;./lib/openmap.jar  -d ./bin/ -deprecation ./src/CacheWolf/*.java 
+javac -cp ./lib/JavaEwe.zip;./lib/ewesoft.zip;./lib/EwesoftRegex.zip;./lib/HTML.zip;./lib/openmap.jar  -d ./bin/ -deprecation ./src/CacheWolf/*.java 
