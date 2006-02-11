034736dfac5ae99a2bd48716047e6f5a22df44fc
kalli <kalli@55547f66-8dd2-3569-90c8-08ced114d07d>
2006-02-11 17:02:47 +0000

    Classpath angepasst, JavaEwe durch CompileEwe ersetzt

--- compile.bat
+++ compile.bat
@@ -1,2 +1,2 @@
 if not exist bin\CacheWolf mkdir bin\CacheWolf
-javac -cp ./lib/JavaEwe.zip;./lib/ewesoft.zip;./lib/EwesoftRegex.zip;./lib/HTML.zip;./lib/openmap.jar  -d ./bin/ -deprecation ./src/CacheWolf/*.java 
+javac -cp ./lib/CompileEwe.zip;./lib/ewesoft.zip;./lib/EwesoftRegex.zip;./lib/HTML.zip;./lib/openmap.jar  -d ./bin/ -deprecation ./src/CacheWolf/*.java 
