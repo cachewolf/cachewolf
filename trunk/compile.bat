cdacdecf2e4fae0eb04753f472772519eb016a6e
kalli <kalli@55547f66-8dd2-3569-90c8-08ced114d07d>
2006-02-11 10:37:08 +0000

    bin/CacheWolf wird erzeugt, wenn es noch nicht da ist. Wurde notwendig, da der Pfad bin/CacheWolf aus dem repos entfernt werden musste

--- trunk/compile.bat
+++ trunk/compile.bat
@@ -1,1 +1,2 @@
+if not exist bin\CacheWolf mkdir bin\CacheWolf
 javac -cp ./lib/JavaEwe.zip;./lib/ewesoft.zip;./lib/EwesoftRegex.zip;./lib/HTML.zip;./lib/openmap.jar  -d ./bin/ -deprecation ./src/CacheWolf/*.java 
