18823dd8a8ce180b2df8565989bcac8ef54d6e44
kalli <kalli@55547f66-8dd2-3569-90c8-08ced114d07d>
2006-02-28 17:02:49 +0000

    Bugfix für PPC: Beim Aufruf der HTML-Dateien für "Über", "Legende" und "WolfLanguage" wird das Programmverzeichnis vor den Dateinamen gesetzt.

--- trunk/src/CacheWolf/Version.java
+++ trunk/src/CacheWolf/Version.java
@@ -3,7 +3,7 @@ package CacheWolf;
 public class Version {
 	static final String VER_MAJOR = "0.9";
 	static final String VER_MINOR = "l";
-	static final String VER_BUILD = " SVN";
+	static final String VER_BUILD = " 2702";
 	
 	public static String getRelease() {
 		return VER_MAJOR + VER_MINOR + " " + VER_BUILD;
