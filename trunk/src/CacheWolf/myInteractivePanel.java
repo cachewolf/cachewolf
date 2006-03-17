6e660dd038f40771fa68eee75f0ab19eeb3b7e7a
bilbowolf <bilbowolf@55547f66-8dd2-3569-90c8-08ced114d07d>
2006-03-17 21:34:45 +0000

    Beliebiges Zoomen im Radar: Nicht fertig!

--- trunk/src/CacheWolf/myInteractivePanel.java
+++ trunk/src/CacheWolf/myInteractivePanel.java
@@ -3,6 +3,7 @@ import ewe.graphics.*;
 import ewe.ui.*;
 import ewe.sys.*;
 import ewe.fx.*;
+import ewe.graphics.*;
 
 /**
 *	This class allows handling of a user click on a cache
@@ -12,6 +13,10 @@ import ewe.fx.*;
 public class myInteractivePanel extends InteractivePanel{
 
 	MainTab mt;
+	
+	boolean penMoving = false;
+	int x1,y1,x2,y2 = 0;
+	static Color RED = new Color(255,0,0);
 	
 	public void imageClicked(AniImage which, Point pos){
 		String fn = new String();
@@ -24,4 +29,39 @@ public class myInteractivePanel extends InteractivePanel{
 	public void setMainTab(MainTab tb){
 		mt = tb;
 	}
+	/*
+	public void onEvent(Event ev){
+		BufferedGraphics bfg;
+		Graphics g;
+		if(ev instanceof PenEvent && ev.type == PenEvent.PEN_DRAG){
+			PenEvent pev = (PenEvent)ev;
+			if(penMoving == false){
+				penMoving = true;
+				x1 = pev.x;
+				y1 = pev.y;
+				x2 = x1;
+				y2 = y1;
+				Vm.debug("Pen starting");
+			} else {
+				bfg = new BufferedGraphics(this.getGraphics(), new Rect(new Dimension(50,50)));
+				g = bfg.getGraphics();
+				g.setDrawOp(Graphics.DRAW_XOR);
+				g.setPen(new Pen(RED,Pen.SOLID,1));
+				g.drawRect(x1,y1,20,20);
+				bfg.release();
+				x2 = pev.x;
+				y2 = pev.y;
+				bfg = new BufferedGraphics(this.getGraphics(), new Rect(new Dimension(50,50)));
+				g = bfg.getGraphics();
+				g.setDrawOp(Graphics.DRAW_XOR);
+				g.setPen(new Pen(RED,Pen.SOLID,1));
+				g.drawRect(x1,y1,x2-x1,y2-y1);
+				bfg.release();
+				Vm.debug("Pen moving");
+			}
+			
+		}
+		super.onEvent(ev);
+	}
+	*/
 }
