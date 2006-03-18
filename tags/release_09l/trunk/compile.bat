28ee8f692e2f6989db80735986b8ae7c242c5d6e
kalli <kalli@55547f66-8dd2-3569-90c8-08ced114d07d>
2006-03-18 12:23:54 +0000

    Removed file/folder

--- tags/release_09l/trunk/compile.bat
+++
@@ -1,2 +0,0 @@
-if not exist bin\CacheWolf mkdir bin\CacheWolf
-javac -cp ./lib/CompileEwe.zip;./lib/ewesoft.zip;./lib/EwesoftRegex.zip;./lib/HTML.zip;./lib/openmap.jar  -d ./bin/ -deprecation ./src/CacheWolf/*.java 
--- tags/release_09l/trunk/runwolf.bat
+++
@@ -1,4 +0,0 @@
-cd work
-java -cp ../lib/ewe.jar;../lib/ewesoft.zip;../lib/EwesoftRegex.zip;../lib/HTML.zip;../lib/openmap.jar;../bin Ewe CacheWolf.CacheWolf
-cd ..
-
