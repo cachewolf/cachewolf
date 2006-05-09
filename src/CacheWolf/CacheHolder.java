46a96b2937fdd4de26a4d568c8793bd843c1be45
bilbowolf <bilbowolf@55547f66-8dd2-3569-90c8-08ced114d07d>
2006-05-09 05:23:18 +0000

    Markieren in der Listenansicht (noch nicht fertig)

--- src/CacheWolf/CacheHolder.java
+++ src/CacheWolf/CacheHolder.java
@@ -58,6 +58,7 @@ public class CacheHolder {
   public boolean is_update = false;
   public boolean is_selected = false;
   public boolean is_HTML = true;
+  public boolean is_Checked = false;
     
   public CacheHolder update(CacheHolder newCh){
 	  // flags
