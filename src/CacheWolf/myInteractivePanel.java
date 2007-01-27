f6347024feea697eda1c5eaa73588cef7cb6daab
salzkammergut <salzkammergut@55547f66-8dd2-3569-90c8-08ced114d07d>
2007-01-27 14:26:46 +0000

    Radar Panel Update: Zoom in bis 1km (fuer hohe Cachedichte in Staedten) Gelber 1km Kreis Cacheinfos beim langen Anklicken (PDA) bzw. Mouseover (PC)

--- src/CacheWolf/myInteractivePanel.java
+++ src/CacheWolf/myInteractivePanel.java
@@ -17,17 +17,65 @@ public class myInteractivePanel extends InteractivePanel{
 	boolean penMoving = false;
 	int x1,y1,x2,y2 = 0;
 	static Color RED = new Color(255,0,0);
+	Font font = new Font("Verdana", Font.BOLD, 10);
+	FontMetrics fm = getFontMetrics();
+	long timePenOn=0;
+	AniImage imgInfo;
+	String strDifficulty=MyLocale.getMsg(1120,"Diff");
+	String strTerrain=MyLocale.getMsg(1121,"Terr");
+	
+	private void clearInfo() {
+		removeImage(imgInfo);
+		imgInfo=null;
+		refresh();
+		onImage=null;
+	}
 	
 	public void imageClicked(AniImage which, Point pos){
-		String fn = new String();
-		if(which instanceof RadarPanelImage){
-			RadarPanelImage ich = (RadarPanelImage)which;
-			mt.selectAndActive(ich.rownum);
+		long timePenOff=Vm.getTimeStampLong();
+		// If the pen rested more than 500 msec, we only display the info and don't treat it as a click
+		if (timePenOff-timePenOn<500 || !Vm.isMobile()) { 
+			String fn = new String();
+			if(which instanceof RadarPanelImage){
+				RadarPanelImage ich = (RadarPanelImage)which;
+				mt.selectAndActive(ich.rownum);
+			}
+		} else {
+			if (imgInfo!=null) clearInfo(); 
 		}
 	}
 	
 	public void setMainTab(MainTab tb){
 		mt = tb;
+	}
+	public boolean imageMovedOn(AniImage which) {
+		timePenOn=Vm.getTimeStampLong();
+		RadarPanelImage imgRP=(RadarPanelImage) which;
+		ewe.util.Vector cacheDB=Global.getProfile().cacheDB;
+		CacheHolder ch=(CacheHolder) cacheDB.get(imgRP.rownum);
+		String s=ch.wayPoint+"  "+ch.CacheSize+" / "+strDifficulty+"="+ch.hard+"  "+strTerrain+"="+ch.terrain;
+		int tw;
+		Image img = new Image(tw=fm.getTextWidth(s)+2,fm.getHeight()+2);
+		Graphics g = new Graphics(img);
+		g.setColor(new Color(0,0,255));
+		g.fillRect(0,0,tw, fm.getHeight()+2);
+		g.setColor(Color.White);
+		g.drawText(s, 1,1);
+		imgInfo = new AniImage(img);
+		Rect r=getVisibleArea(null);
+		imgInfo.setLocation(r.x,r.y); // Place the info at top left corner
+		imgInfo.properties = mImage.IsNotHot;
+		addImage(imgInfo);
+		refreshOnScreen(imgInfo);
+		return true;
+	}
+	public boolean imageMovedOff(AniImage which) {
+		clearInfo();
+		return true;
+	}
+	public void onPenEvent(PenEvent ev) {
+		super.onPenEvent(ev);
+		if (ev.type==PenEvent.PEN_UP) clearInfo();
 	}
 	/*
 	public void onEvent(Event ev){
