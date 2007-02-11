e068c8584148b362639384bf0ba323f5c3a66d9b
pfefferCode <prog@robert-arnold.de>
2007-02-11 23:52:51 +0000

    InfoBox sieht jetzt wieder so aus, wie vor den commits von mir: wieder alles zentriert

--- src/CacheWolf/InfoBox.java
+++ src/CacheWolf/InfoBox.java
@@ -42,11 +42,13 @@ public class InfoBox extends Form{
 
 	public InfoBox(String title, String info, int ty){
 		this(title, info, ty, true);
+		this.setPreferredSize(170, 50);
+		relayout(false);
 	}
 
 	public InfoBox(String title, String info, int ty, boolean autoWrap) {
 		type = ty;
-		this.setPreferredSize(150,80);
+		this.setPreferredSize(150,50);
 		this.title = title;
 		switch (type) {
 		case CHECKBOX: 
@@ -61,12 +63,14 @@ public class InfoBox extends Form{
 		case DISPLAY_ONLY:
 			msgArea = new TextMessage(info);
 			msgArea.autoWrap = autoWrap;
+			msgArea.alignment = Gui.CENTER;
+			msgArea.anchor = Gui.CENTER;
 			this.addLast(msgArea.getScrollablePanel(), CellConstants.STRETCH, CellConstants.FILL);
 			break;
 		case PROGRESS_WITH_WARNINGS:
 			msgArea = new TextMessage(info);
 			msgArea.autoWrap = autoWrap;
-			msgArea.setPreferredSize(150, 80);
+			msgArea.setPreferredSize(150, 50);
 			this.addLast(msgArea.getScrollablePanel(), CellConstants.HEXPAND | CellConstants.HGROW, CellConstants.HEXPAND | CellConstants.HGROW);
 			warnings = new TextMessage("");
 			warnings.autoWrap = autoWrap;
