9223d9a928218557c54c55573b331a3549f0b21a
bilbowolf <bilbowolf@55547f66-8dd2-3569-90c8-08ced114d07d>
2007-05-30 21:44:56 +0000

    Änderungen am spider, um die Layoutänderung von gc.com vom 30.05.07 zu parsen.

--- src/CacheWolf/Version.java
+++ src/CacheWolf/Version.java
@@ -8,7 +8,7 @@ import ewe.sys.*;
 
 public class Version {
 	static final String VER_MAJOR = "0.9n";
-	static final String VER_MINOR = "";
+	static final String VER_MINOR = " patch 1";
 	static final String VER_BUILD = " RC ";
 	static final String VER_SVN ="$LastChangedRevision$"; //the  number is automatically replaced by subversion to the latest versionnumer of this file (svn:keywords LastChangedRevision)
 	
