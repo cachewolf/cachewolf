bb26ab44e86653aa02b7bdede89646f0f0d85b72
salzkammergut <salzkammergut@55547f66-8dd2-3569-90c8-08ced114d07d>
2007-06-30 09:38:24 +0000

    Attribute: Update mit Texten zu den haeufigsten Attributen

--- src/CacheWolf/Attribute.java
+++ src/CacheWolf/Attribute.java
@@ -21,7 +21,7 @@ public class Attribute {
 	 */
 	private String [] attributeNames= {
 			"error.gif",			//00 Unknown attribute
-			"available-no.gif", 	//01
+			"available-no.gif", 	//01 not 24-7
 			"available-yes.gif", 	//02 available 24-7
 			"bicycles-no.gif", 		//03 no bikes
 			"bicycles-yes.gif", 	//04 bikes allowed
@@ -30,66 +30,66 @@ public class Attribute {
 			"cactus-no.gif", 		//07
 			"cactus-yes.gif",		//08
 			"campfires-no.gif", 	//09 no campfires
-			"campfires-yes.gif", 	//10
+			"campfires-yes.gif", 	//10 campfires allowed
 			"camping-no.gif", 		//11
 			"camping-yes.gif", 		//12
 			"cliff-no.gif", 		//13
-			"cliff-yes.gif", 		//14
-			"climbing-no.gif", 		//15
-			"climbing-yes.gif", 	//16
-			"compass-no.gif", 		//17
+			"cliff-yes.gif", 		//14 falling-rocks nearby
+			"climbing-no.gif", 		//15 no difficult climbing
+			"climbing-yes.gif", 	//16 difficult climbing
+			"compass-no.gif", 		//17 
 			"compass-yes.gif",		//18
 			"cow-no.gif", 			//19
-			"cow-yes.gif", 			//20
+			"cow-yes.gif", 			//20 watch for livestock
 			"danger-no.gif", 		//21
-			"danger-yes.gif", 		//22
-			"dogs-no.gif", 			//23
+			"danger-yes.gif", 		//22 dangerous area
+			"dogs-no.gif", 			//23 no dogs
 			"dogs-yes.gif", 		//24 dogs allowed
 			"fee-no.gif", 			//25
-			"fee-yes.gif", 			//26
+			"fee-yes.gif", 			//26 access/parking fees
 			"hiking-no.gif", 		//27
-			"hiking-yes.gif", 		//28
+			"hiking-yes.gif", 		//28 significant hike
 			"horses-no.gif", 		//29 no horses
-			"horses-yes.gif", 		//30
+			"horses-yes.gif", 		//30 horses allowed
 			"hunting-no.gif", 		//31
-			"hunting-yes.gif", 		//32
-			"jeeps-no.gif", 		//33
-			"jeeps-yes.gif", 		//34
-			"kids-no.gif", 			//35
+			"hunting-yes.gif", 		//32 hunting area
+			"jeeps-no.gif", 		//33 no off-road vehicles
+			"jeeps-yes.gif", 		//34 off-road vehicles allowed
+			"kids-no.gif", 			//35 no kids
 			"kids-yes.gif", 		//36 kid friendly
 			"mine-no.gif", 			//37
 			"mine-yes.gif", 		//38
-			"motorcycles-no.gif", 	//39
-			"motorcycles-yes.gif", 	//40
+			"motorcycles-no.gif", 	//39 no motorcycles
+			"motorcycles-yes.gif", 	//40 motorcycles allowed
 			"night-no.gif", 		//41 not recommended at night
-			"night-yes.gif", 		//42
+			"night-yes.gif", 		//42 recommended at night
 			"onehour-no.gif", 		//43 takes more than one hour
 			"onehour-yes.gif", 		//44 takes less than one hour
 			"parking-no.gif", 		//45
 			"parking-yes.gif", 		//46 parking available
 			"phone-no.gif", 		//47
-			"phone-yes.gif", 		//48
+			"phone-yes.gif", 		//48 telephone nearby
 			"picnic-no.gif", 		//49
-			"picnic-yes.gif", 		//50
+			"picnic-yes.gif", 		//50 picnic tables available
 			"poisonoak-no.gif", 	//51
 			"poisonoak-yes.gif", 	//52
 			"public-no.gif", 		//53
 			"public-yes.gif", 		//54 public transit available
-			"quads-no.gif", 		//55
+			"quads-no.gif", 		//55 no quads
 			"quads-yes.gif", 		//56 quads allowed
 			"rappelling-no.gif", 	//57
-			"rappelling-yes.gif", 	//58
+			"rappelling-yes.gif", 	//58 climbing gear
 			"restrooms-no.gif", 	//59
-			"restrooms-yes.gif", 	//60
+			"restrooms-yes.gif", 	//60 restrooms available
 			"scenic-no.gif", 		//61
 			"scenic-yes.gif", 		//62 scenic view
 			"scuba-no.gif", 		//63
 			"scuba-yes.gif", 		//64
 			"snakes-no.gif", 		//65
 			"snakes-yes.gif", 		//66
-			"snowmobiles-no.gif", 	//67
+			"snowmobiles-no.gif", 	//67 no snowmobiles
 			"snowmobiles-yes.gif", 	//68
-			"stealth-no.gif", 		//69
+			"stealth-no.gif", 		//69 no stealth required
 			"stealth-yes.gif", 		//70 stealth required
 			"stroller-no.gif",		//71 not stroller accessible
 			"stroller-yes.gif", 	//72 stroller accessible
@@ -100,11 +100,11 @@ public class Attribute {
 			"ticks-no.gif", 		//77
 			"ticks-yes.gif", 		//78 ticks!
 			"wading-no.gif", 		//79
-			"wading-yes.gif", 		//80
+			"wading-yes.gif", 		//80 may require wading
 			"water-no.gif", 		//81
-			"water-yes.gif", 		//82
+			"water-yes.gif", 		//82 drinking water nearby
 			"wheelchair-no.gif", 	//83 not wheelchair accessible
-			"wheelchair-yes.gif", 	//84
+			"wheelchair-yes.gif", 	//84 wheelchair accessible
 			"winter-no.gif", 		//85 not available for winter
 			"winter-yes.gif"}; 		//86 available in winter
 	private mImage [] attributeImages=new mImage[87];
