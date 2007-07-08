1ed5f12f62cb718312b456d08112472252de4eee
bilbowolf <bilbowolf@55547f66-8dd2-3569-90c8-08ced114d07d>
2007-07-08 08:00:25 +0000

    0.9n Patch 4 versionierung

--- branches/bugfix_09n/src/CacheWolf/Version.java
+++ branches/bugfix_09n/src/CacheWolf/Version.java
@@ -8,7 +8,7 @@ import ewe.sys.*;
 
 public class Version {
 	static final String VER_MAJOR = "0.9n";
-	static final String VER_MINOR = " patch 3";
+	static final String VER_MINOR = " patch 4";
 	static final String VER_BUILD = " RC ";
 	static final String VER_SVN ="$LastChangedRevision$"; //the  number is automatically replaced by subversion to the latest versionnumer of this file (svn:keywords LastChangedRevision)
 	
