03016689f1b6fbd422ae57a0423c9cdc323009a2
pfeffer <pfeffer@55547f66-8dd2-3569-90c8-08ced114d07d>
2007-08-14 11:20:56 +0000

    added utils to compile.bat and .sh

--- trunk/compile.bat
+++ trunk/compile.bat
@@ -1,3 +1,3 @@
 if not exist bin\CacheWolf mkdir bin\CacheWolf
 if not exist bin\exp mkdir bin\exp
-javac -cp ./lib/CompileEwe.zip;./lib/ewesoft.zip;./lib/EwesoftRegex.zip;./lib/HTML.zip;./lib/openmap.jar  -d ./bin/ -deprecation ./src/CacheWolf/*.java ./src/exp/*.java 
+javac -cp ./lib/CompileEwe.zip;./lib/ewesoft.zip;./lib/EwesoftRegex.zip;./lib/HTML.zip;./lib/openmap.jar  -d ./bin/ -deprecation ./src/CacheWolf/*.java ./src/exp/*.java ./src/utils/*.java
