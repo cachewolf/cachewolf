239f4f39a493fb140a82626573d1d08f222747b7
pfefferCode <prog@robert-arnold.de>
2007-08-14 01:13:30 +0000

    es gab noch einen Fehler in meinem letzten Fix :-(

--- src/utils/FileBugfix.java
+++ src/utils/FileBugfix.java
@@ -16,9 +16,9 @@ public class FileBugfix extends File{
 	
 	public String[] list(final String mask,final int listAndSortOptions)
 	{
-		if ((listAndSortOptions & FileBase.LIST_FILES_ONLY) > 0)
-		  return super.listMultiple(mask+",*.xyzq3246wwee345rrtzih6ljbkoih", listAndSortOptions); // with only one "*" listmultiple calls list
-		else return super.list(mask, listAndSortOptions);
+		if ( ((listAndSortOptions & FileBase.LIST_FILES_ONLY) == 0) || mask == null || mask == "*.*" ) // in case of dirs listmuliple calls list even if there are more than one "*" in the mask -> avoid endless recursion //TODO test if lis works on all systems when there are more than one dot in the dirname 
+			return super.list(mask, listAndSortOptions); 
+		return super.listMultiple(mask+",*.xyzq3246wwee345rrtzih6ljbkoih", listAndSortOptions); // with only one "*" listmultiple calls list
 		/*
 		 * super.list has on some systems (linux, but also Windows Mobile 2003) a bug 
 		 * in ewe 1.49: 
