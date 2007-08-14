cd7303b7e9c994ca503cb5fd0df6d1c63f7c4b28
pfefferCode <prog@robert-arnold.de>
2007-08-14 17:50:30 +0000

    fixed: on some systems a stack overflow occured in my fix from last night

--- src/utils/FileBugfix.java
+++ src/utils/FileBugfix.java
@@ -4,6 +4,8 @@
 package utils;
 
 import ewe.io.*;
+import ewe.util.FileComparer;
+import ewe.util.mString;
 
 /**
  * @author pfeffer
@@ -12,13 +14,11 @@ import ewe.io.*;
  */
 public class FileBugfix extends File{
 	public FileBugfix(String path) 
-		{super(path);}
-	
+	{super(path);}
+
 	public String[] list(final String mask,final int listAndSortOptions)
 	{
-		if ( ((listAndSortOptions & FileBase.LIST_FILES_ONLY) == 0) || mask == null || mask == "*.*" ) // in case of dirs listmuliple calls list even if there are more than one "*" in the mask -> avoid endless recursion //TODO test if lis works on all systems when there are more than one dot in the dirname 
-			return super.list(mask, listAndSortOptions); 
-		return super.listMultiple(mask+",*.xyzq3246wwee345rrtzih6ljbkoih", listAndSortOptions); // with only one "*" listmultiple calls list
+		return listBugFixed(mask, listAndSortOptions);
 		/*
 		 * super.list has on some systems (linux, but also Windows Mobile 2003) a bug 
 		 * in ewe 1.49: 
@@ -27,5 +27,43 @@ public class FileBugfix extends File{
 		 * listmultiple doesn't have this bug 
 		 */
 	}
+	public String[] listBugFixed(final String compositeMask,final int listAndSortOptions) {
+		String mask = (compositeMask == null) ? "*.*" : compositeMask; 
+		String[] found; //the following code is mainly copoed from FileBase.listmultiple to avoid recursion it is not called
+		char c = mask.indexOf(',') == -1 ? ';' : ',';
+		String masks [] = mString.split(mask,c);
+		String dirs [] = new String[0];
+		if ((listAndSortOptions & LIST_FILES_ONLY) == 0)
+			dirs = super.list(null,LIST_DIRECTORIES_ONLY);
+		if ((listAndSortOptions & LIST_DIRECTORIES_ONLY) == 0)
+			found = super.list(null,File.LIST_FILES_ONLY|listAndSortOptions);
+		else
+			found = dirs;
 
+		ewe.util.FileComparer [] fcs = new ewe.util.FileComparer[masks.length];
+
+		for (int i = 0; i<masks.length; i++)
+			fcs[i] = new FileComparer((File)this,ewe.sys.Vm.getLocale(),listAndSortOptions,masks[i]);
+		int left = found.length;
+		for (int i = 0; i<found.length; i++){
+			boolean matched = false;
+			for (int f = 0; f<fcs.length; f++){
+				if (fcs[f].matches(found[i])){
+					matched = true;
+					break;
+				}
+			}
+			if (!matched) {
+				found[i] = null;
+				left--;
+			}
+		}
+		String [] isMatching = new String[dirs.length+left];
+		ewe.sys.Vm.copyArray(dirs,0,isMatching,0,dirs.length);
+		for (int i = 0, d = dirs.length; i<found.length; i++)
+			if (found[i] != null)
+				isMatching[d++] = found[i];
+		found = isMatching;
+		return found;
+	}
 }
