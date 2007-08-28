2b8e936ca3027e472741abe230b9d900300e29a5
mirabilos <mirabilos@55547f66-8dd2-3569-90c8-08ced114d07d>
2007-08-28 12:14:40 +0000

    first commit, just something simple so I see that it works: • change the shebang line from /bin/bash to /bin/sh on all shell scripts   to improve portability • whitespace cleanup • set the svn:executable property on the scripts

--- trunk/compile.sh
+++ trunk/compile.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#!/bin/sh
 javac -cp ./lib/CompileEwe.zip:./lib/ewesoft.zip:./lib/EwesoftRegex.zip:./lib/HTML.zip:./lib/openmap.jar  -d ./bin/ -deprecation -nowarn  ./src/CacheWolf/*.java ./src/exp/*.java ./src/utils/*.java
