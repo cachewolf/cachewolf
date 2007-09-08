56d1e33c8c6d72053d00c55ef2d6f887f75a8e09
pfeffer <pfeffer@55547f66-8dd2-3569-90c8-08ced114d07d>
2007-09-08 18:03:49 +0000

    set the first focus to the cache list instead of the main menu

--- trunk/src/CacheWolf/TableForm.java
+++ trunk/src/CacheWolf/TableForm.java
@@ -2,6 +2,8 @@ package CacheWolf;
 
 import ewe.ui.CellConstants;
 import ewe.ui.CellPanel;
+import ewe.ui.Control;
+import ewe.ui.ControlEvent;
 import ewe.ui.Editor;
 
 public class TableForm extends Editor {
@@ -13,6 +15,7 @@ public class TableForm extends Editor {
 			this.addLast(tp,STRETCH,FILL);
 			this.addLast(Global.mainForm.mMenu,CellConstants.DONTSTRETCH, CellConstants.FILL);
 		}
+		this.firstFocus = tp; // give the first fokus to the list of caches, not to the main menu
 		/*
 		CellPanel[] menuList = addToolbar();
 		menuList[0].addLast(Global.mainForm.mMenu);
