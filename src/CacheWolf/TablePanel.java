754acf181f1473e4373f50a0b0ba0024f18ede21
salzkammergut <salzkammergut@55547f66-8dd2-3569-90c8-08ced114d07d>
2007-09-09 21:16:26 +0000

    Listenansicht: Verbesserung der Darstellung bei Navigation mittels Tastatur. Wenn eine Spalte angeklickt wurde bleibt sie dann beim Scrollen mittels Tastatur auch weiterhin sichtbar. Fix: Die letzte Spalte wurde manchmal nicht grau dargestellt

--- src/CacheWolf/TablePanel.java
+++ src/CacheWolf/TablePanel.java
@@ -41,46 +41,18 @@ public class TablePanel extends CellPanel{
 		tc.setTableModel(myMod);
 	}
 	
-	public void setSelectedCache(int row){ 
-	/*	selectedCh=null;
-		selectedIdx=-1;
-		if (row>=0 && row<cacheDB.size())  {
-			selectedCh=(CacheHolder) cacheDB.get(row);
-			selectedIdx=row;
-		} 		
-	*/
-	}
-	
 	/** Mark the row as selected so that myTableModel can color it grey */
 	public void selectRow(int row) {
-		setSelectedCache(row);
-//		tc.addToSelection(row,0); 
-//		tc.addToSelection(row,myMod.MAXCOLUMNS-1);
 		// Ensure that the highlighted row is visible (e.g. when coming from radar panel)
-		tc.scrollToVisible(row,0);
 		// Next line needed for key scrolling 
-		tc.cursorTo(row, tc.cursor.x+tc.listMode, true);
-// <<<<<<< .mine
-		//tc.scrollToVisible(row,0);
-		//tc.clearSelection(null);
-		//tc.addToSelection(row,0); 
-		//tc.addToSelection(row,myMod.MAXCOLUMNS-1); 
-		//tc.scrollToVisible(row,0);
-		/*tc.clearSelection(null);
-		tc.addToSelection(row,0); 
-		tc.addToSelection(row,myMod.MAXCOLUMNS-1);*/ 
-		//tc.paintSelectedCells();
-//=======
-//>>>>>>> .r820
+		tc.cursorTo(row, tc.cursor.x+tc.listMode, true); 
 	}
 	
 	/** Highlight the first row in grey. It can be unhighlighted by clicking */
 	public void selectFirstRow() {
 		myMod.cursorSize=new Dimension(-1,1);
 		if (cacheDB.size()>0) {
-			tc.cursorTo(0, -1, true);
-//			tc.addToSelection(0,0); 
-//			tc.addToSelection(0,myMod.MAXCOLUMNS-1);
+			tc.cursorTo(0, 0, true);
 		}
 	}
 	
@@ -90,20 +62,6 @@ public class TablePanel extends CellPanel{
 	 */
 	public int getSelectedCache(){
 		return tc.cursor.y;
-		/*
-		// If cacheDB is empty return -1, cannot select a cache
-		if (cacheDB.size()==0) return -1;
-		// If cacheDB has entries, but all are filtered, return -1
-		if (((CacheHolder)cacheDB.get(0)).is_filtered) return -1;
-		// Now we have at least one visible cache
-		// Check whether selected cache was deleted or filtered 
-		if (selectedIdx>=Global.mainTab.tbP.myMod.numRows) return Global.mainTab.tbP.myMod.numRows-1;
-		// We had a previously selected cache, check whether it is now filtered
-		if (selectedCh==null || selectedCh.is_filtered) return 0; // Return first visible cache
-		// Check whether the order of the list has changed because of sort/filter/search operations
-		if (cacheDB.get(selectedIdx)==selectedCh) return selectedIdx;
-		// The position has changed, return the new position
-		return cacheDB.find(selectedCh); */
 	}
 	
 	public void saveColWidth(Preferences pref){
@@ -114,15 +72,7 @@ public class TablePanel extends CellPanel{
 		}
 	}
 	
-	/*
-	public void selectAndActive(int rownum){
-		//		tc.scrollToVisible(rownum, 0);
-		selectRow(rownum);  // color it in grey
-	}
-	*/
-	
 	public void resetModel() {
-		setSelectedCache(0);
 		myMod.numRows = cacheDB.size();
 		Global.getProfile().updateBearingDistance();
 		Filter flt = new Filter();
@@ -140,7 +90,7 @@ public class TablePanel extends CellPanel{
 		tc.update(true);
 	}
 	
-	/** Move all filtered caches to the end of the table and redesplay table */
+	/** Move all filtered caches to the end of the table and redisplay table */
 	//TODO Add a sort here to restore the sort after a filter
 	public void refreshTable(){
 		myMod.updateRows();
