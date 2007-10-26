929756df31d88a6f851abab474499950aeb6a697
mirabilos <mirabilos@55547f66-8dd2-3569-90c8-08ced114d07d>
2007-10-26 16:44:49 +0000

    next round of fixes

--- trunk/fwrtsnapshot.sh
+++ trunk/fwrtsnapshot.sh
@@ -8,17 +8,17 @@ javac -cp ./lib/CompileEwe.zip:./lib/  -d ./bin/ -deprecation -nowarn ./src/Cach
 /usr/local/bin/ewecl programs/Jewel.ewe -c cwberlios.jnf
 rm -rf published
 mkdir -p published/attributes
-cd programs/CacheWolf && mv * ../../published/
+mv programs/CacheWolf/* published/
 chmod 755 published/*
 chmod 644 published/*/*
 install -c -m 644 work/CacheWolf.ewe published/
 install -c -m 644 resources/*.{def,html,tpl,zip} published/
 install -c -m 644 resources/attributes-big/*.gif published/attributes/
-mkdir -p ~/public_html/CacheWolf-BE/r$v
-cd published && \
+(cd published && \
     find *.{def,html,tpl,zip} attributes/*.gif -type f | sort | \
-    cpio -oC512 -Hustar -Mdist | gzip -n9 >datfiles.tgz && \
+    /usr/local/bin/cpio -oC512 -Hustar -Mdist | gzip -n9 >datfiles.tgz && \
     rm -rf *.{def,html,tpl,zip} attributes && \
-    chmod 644 datfiles.tgz && \
-    mv * ~/public_html/CacheWolf-BE/r$v/
+    chmod 644 datfiles.tgz)
+mkdir -p ~/public_html/CacheWolf-BE/r$v
+mv published/* ~/public_html/CacheWolf-BE/r$v/
 rm -rf published
