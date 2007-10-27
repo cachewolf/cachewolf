eb5e543e573c6a0622a43a1e7bd0323ce78182e7
mirabilos <mirabilos@55547f66-8dd2-3569-90c8-08ced114d07d>
2007-10-27 01:26:48 +0000

    according to MiK, certain files, namely • icon_*.gif • these referenced by *.html must not be placed in the .ewe file either.
    Move all files (but not (yet) the attributes*/ dirs) that
    should not be placed in the .ewe to the new directory
    res_noewe/ and adjust the getRes.{bat,sh} and autobuilder
    scripts accordingly

--- trunk/fwrtsnapshot.sh
+++ trunk/fwrtsnapshot.sh
@@ -8,19 +8,19 @@ printf '/VER_SVN =/s/\$.*\$/$LastChangedRevision: %s $/\nwq\n' $v | \
 javac -cp ./lib/CompileEwe.zip:./lib/  -d ./bin/ -deprecation -nowarn ./src/CacheWolf/*.java ./src/CacheWolf/*/*.java ./src/exp/*.java ./src/utils/*.java
 /usr/local/bin/ewecl programs/Jewel.ewe -c cwberlios.jnf
 rm -rf published
-mkdir -p published/attributes
+mkdir -p published/dat/attributes
 mv programs/CacheWolf/* published/
 chmod 755 published/*
 chmod 644 published/*/*
+chmod 755 published/dat/attributes
 printf '1,$g/ 12M/s///\nwq\n' | ed -s published/Jar/CacheWolf.bat
 install -c -m 644 work/CacheWolf.ewe published/
-install -c -m 644 resources/*.{def,html,tpl,zip} published/
-install -c -m 644 resources/attributes-big/*.gif published/attributes/
-(cd published && \
-    find *.{def,html,tpl,zip} attributes/*.gif -type f | sort | \
-    /usr/local/bin/cpio -oC512 -Hustar -Mdist >datfiles.tar && \
-    rm -rf *.{def,html,tpl,zip} attributes && \
-    chmod 644 datfiles.tgz)
+install -c -m 644 res_noewe/* published/dat/
+install -c -m 644 resources/attributes-big/*.gif published/dat/attributes/
+(cd published/dat && find * -type f | sort | \
+    /usr/local/bin/cpio -oC512 -Hustar -Mdist >../datfiles.tar)
+rm -rf published/dat
+chmod 644 published/datfiles.tar
 mkdir -p ~/public_html/CacheWolf-BE/r$v
 mv published/* ~/public_html/CacheWolf-BE/r$v/
 rm -rf published
