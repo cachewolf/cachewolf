47d018eee075e5e2cdc3e6b71d304b7cab63648e
pfeffer <pfeffer@55547f66-8dd2-3569-90c8-08ced114d07d>
2007-10-31 15:09:59 +0000

    Gauß-Krüger-Transformation: I forgot to commit this file yesterday

--- trunk/src/CacheWolf/Matrix.java
+++ trunk/src/CacheWolf/Matrix.java
@@ -49,6 +49,18 @@ public class Matrix{
 		return product;
 	}
 	
+	public void MultiplyByScalar (double f) {
+		for (int i = 0; i < matrix.length; i++)
+			for (int j = 0; j < matrix[0].length; j++)
+				matrix[i][j] = matrix[i][j] * f;
+	}
+	
+	public void add (Matrix a) {
+		for (int i = 0; i < matrix.length; i++)
+			for (int j = 0; j < matrix[0].length; j++)
+				matrix[i][j] = matrix[i][j] + a.matrix[i][j];
+	}
+	
 	/**
 	*	Method to transpose a matrix
 	*	example:	| 1 2 |
