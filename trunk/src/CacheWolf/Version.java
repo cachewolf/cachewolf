75872789748d0d3068f2c8a9a54630b2df568c19
pfeffer <pfeffer@55547f66-8dd2-3569-90c8-08ced114d07d>
2008-01-05 21:05:19 +0000

    Version check:  show sand watch while fetching currentversion info, messagebox size: big enough, another localisation

--- trunk/src/CacheWolf/Version.java
+++ trunk/src/CacheWolf/Version.java
@@ -19,13 +19,14 @@ public class Version {
 	static final int VER_MINOR = 9;
 	static final String VER_SVN ="$LastChangedRevision$"; // the number is automatically replaced by subversion to the latest versionnumer of this file (svn:keywords LastChangedRevision)
 	static final int SVN_REVISION = Common.parseInt(VER_SVN.substring(VER_SVN.indexOf(" ")+1, VER_SVN.lastIndexOf(" ")));
-	static final int VERSION_TYPE = 1;
+	static final int VERSION_TYPE = 3;
 	public static final String[] VERSION_TYPES = {
 		"Release", 
 		"ReleaseCandidate",
 		"InDevelopmentStable",
 		"InDevelopmentNewest"
 	};
+	
 	/** only valid after calling checkForUpdates() */
 	static int[] updateavailabe = {0,0,0,0,0}; 
 
@@ -78,7 +79,7 @@ public class Version {
 			checkForUpdates();
 			return MyLocale.getMsg(7022, "Version type") +"\n"+ newVersionsArrayToString();
 		} catch (IOException e) {
-			return "Error getting current version information\n"+e.getMessage();
+			return MyLocale.getMsg(7023, "Error getting current version information") +"\n" + e.getMessage();
 		}
 	}
 
