8781ebc757c52801295d24a1d1f3f20683d3864c
kappler <kappler@55547f66-8dd2-3569-90c8-08ced114d07d>
2008-04-24 05:55:34 +0000

    Fix bug in rebuilding the cache-index

--- trunk/src/CacheWolf/Rebuild.java
+++ trunk/src/CacheWolf/Rebuild.java
@@ -26,10 +26,10 @@ public class Rebuild {
 		int orphans=0; // xml Files without entry in database
 		int nAdded=0;  // caches added to database
 		for (i=0; i<xmlFiles.length; i++) {
-			int pos=xmlFiles[i].indexOf('.');
+			int pos=xmlFiles[i].lastIndexOf('.');
 			if (pos<0) continue;
-			String wayPoint=xmlFiles[i].substring(0,pos);
-			if (wayPoint.charAt(0)=='i' || 			// Check for index.xml and index.bak
+			String wayPoint=xmlFiles[i].substring(0,pos).toUpperCase();
+			if (wayPoint.charAt(0)=='I' || 			// Check for index.xml and index.bak
 				prof.getCacheIndex(wayPoint)>=0)		// Check for waypoints already in database 
 				xmlFiles[i]=null;   				// Remove existing caches or index.xml
 			else {
