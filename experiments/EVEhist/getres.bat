34a291b20c6a04cacf0b09a5ceb90282375df06f
pfeffer <pfeffer@55547f66-8dd2-3569-90c8-08ced114d07d>
2008-10-28 00:42:27 +0000

    EVE: add some config files for compiling

---
+++ experiments/EVEhist/getres.bat
@@ -0,0 +1,17 @@
+mkdir work\attributes
+mkdir work\attributes-big
+copy resources\attributes\*.gif work\attributes
+copy resources\attributes-big\*.gif work\attributes-big
+copy resources\*.gif work
+copy resources\*.png work
+mkdir work\languages
+mkdir work\webmapservices
+copy res_noewe\languages\*.cfg work\languages
+copy res_noewe\webmapservices\*.wms work\webmapservices
+copy res_noewe\webmapservices\*.txt work\webmapservices
+copy res_noewe\*.def work
+copy res_noewe\*.html work
+copy res_noewe\*.png work
+copy res_noewe\*.gif work
+copy res_noewe\*.tpl work
+copy res_noewe\*.zip work
