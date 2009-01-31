07ec9d11f4a0ba6cd9adb1fafc432456e1826c7b
natureshadow <natureshadow@55547f66-8dd2-3569-90c8-08ced114d07d>
2009-01-31 13:08:39 +0000

    First dratft of snapshot script. Updated EveMaker toolchain.

---
+++ experiments/EVEhist/getres.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+mkdir work/attributes
+mkdir work/attributes-big
+cp resources/attributes\*.gif work/attributes
+cp resources/attributes-big\*.gif work/attributes-big
+cp resources/*.gif work
+cp resources/*.png work
+mkdir work/languages
+mkdir work/webmapservices
+cp res_noewe/languages\*.cfg work/languages
+cp res_noewe/webmapservices\*.wms work/webmapservices
+cp res_noewe/webmapservices\*.txt work/webmapservices
+cp res_noewe/*.def work
+cp res_noewe/*.html work
+cp res_noewe/*.png work
+cp res_noewe/*.gif work
+cp res_noewe/*.tpl work
+cp res_noewe/*.zip work
---
+++ experiments/EVEhist/programs/snapshot.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+if [ ! -e snapshot.sh ]; then
+ echo "This script must be run from inside the programs/ directory!"
+ exit 1
+fi
+
+cd ..
+
+echo Updating SVN ...
+svn update
+
+echo Updating version number in code ...
+v=$(svn info | sed -n '/Revision: /s///p')
+printf '/VER_SVN =/s/\$.*\$/$LastChangedRevision: %s $/\nwq\n' $v | \
+    ed -s src/cachewolf/Version.java
+
+echo Removing old build ...
+rm -rf bin
+
+echo Compiling Java classes ...
+mkdir -p bin/cachewolf
+javac -source 1.3 -target 1.1 -encoding windows-1252 \
+    -cp lib/CompileEve.zip:lib/JavaEve.zip:lib/lib-folder -d bin -deprecation -nowarn \
+    src/cachewolf/*.java src/cachewolf/*/*.java
+
+echo Linking executables ...
+cd programs
+java -cp evecl-gui.jar Eve EveMaker.eve -c ../deploy/cw-eve-pc.enf
+java -cp evecl-gui.jar Eve EveMaker.eve -c ../deploy/cw-eve-pda.enf
+
+#mkdir -p published/dat/attributes
+#mkdir -p published/dat/webmapservices
+#mkdir -p published/dat/languages
+#mv programs/CacheWolf/* published/
+#chmod 755 published/*
+#cp lib/java_ewe.dll published/Jar/
+#chmod 644 published/*/*
+#chmod 755 published/dat/attributes
+#chmod 755 published/dat/webmapservices
+#chmod 755 published/dat/languages
+#printf '1,$g/ 12M/s///\nwq\n' | ed -s published/Jar/CacheWolf.bat
+#install -c -m 644 work/CacheWolf.ewe published/
+#install -c -m 644 res_noewe/* published/dat/
+#install -c -m 644 resources/attributes-big/*.gif published/dat/attributes/
+#install -c -m 644 resources/attributes/*-non.gif published/dat/attributes/
+#install -c -m 644 res_noewe/webmapservices/* published/dat/webmapservices/
+#install -c -m 644 res_noewe/languages/* published/dat/languages/
+#(
+#	cd published/dat
+#	find * -type f | sort >../flst
+#	$CPIO -oC512 -Hustar -Mdist <../flst | gzip -n9 >../datfiles.tgz
+#	zip -X ../datfiles.zip -@ <../flst
+#)
+#rm -rf published/dat published/flst
+#chmod 644 published/datfiles.tgz published/datfiles.zip
+#mkdir -p $HOME/public_html/CacheWolf-BE/r$v
+#mv published/* $HOME/public_html/CacheWolf-BE/r$v/
+#rm -rf bin published
