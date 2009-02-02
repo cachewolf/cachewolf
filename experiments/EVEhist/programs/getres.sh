df03ae9cf625cc17fbad9248358b597e9886b475
engywuck <engywuck@55547f66-8dd2-3569-90c8-08ced114d07d>
2009-02-02 18:45:30 +0000

    Need to change relative paths, too.

--- experiments/EVEhist/programs/getres.sh
+++ experiments/EVEhist/programs/getres.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-mkdir -p work/attributes work/attributes-big \
-    work/languages work/webmapservices
-cp resources/*.gif work/
-cp resources/*.png work/
-cp res_noewe/languages/*.cfg work/languages/
-cp res_noewe/webmapservices/*.wms work/webmapservices/
-cp res_noewe/webmapservices/*.txt work/webmapservices/
-cp res_noewe/attributes/*.gif work/attributes/
-cp res_noewe/attributes-big/*.gif work/attributes-big/
-cp res_noewe/*.def work/
-cp res_noewe/*.html work/
-cp res_noewe/*.png work/
-cp res_noewe/*.gif work/
-cp res_noewe/*.tpl work/
-cp res_noewe/*.zip work/
+mkdir -p ../work/attributes ../work/attributes-big \
+    ../work/languages ../work/webmapservices
+cp ../resources/*.gif ../work/
+cp ../resources/*.png ../work/
+cp ../res_noewe/languages/*.cfg ../work/languages/
+cp ../res_noewe/webmapservices/*.wms ../work/webmapservices/
+cp ../res_noewe/webmapservices/*.txt ../work/webmapservices/
+cp ../res_noewe/attributes/*.gif ../work/attributes/
+cp ../res_noewe/attributes-big/*.gif ../work/attributes-big/
+cp ../res_noewe/*.def ../work/
+cp ../res_noewe/*.html ../work/
+cp ../res_noewe/*.png ../work/
+cp ../res_noewe/*.gif ../work/
+cp ../res_noewe/*.tpl ../work/
+cp ../res_noewe/*.zip ../work/
