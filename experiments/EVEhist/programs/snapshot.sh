1597a495ca4de9bbc5b74d4633281dfc52237a6b
natureshadow <natureshadow@55547f66-8dd2-3569-90c8-08ced114d07d>
2009-02-03 21:15:15 +0000

    Snapshot script works now.

--- experiments/EVEhist/programs/snapshot.sh
+++ experiments/EVEhist/programs/snapshot.sh
@@ -30,31 +30,32 @@ cd programs
 java -cp evecl-gui.jar Eve EveMaker.eve -c ../deploy/cw-eve-pc.enf
 java -cp evecl-gui.jar Eve EveMaker.eve -c ../deploy/cw-eve-pda.enf
 
-#mkdir -p published/dat/attributes
-#mkdir -p published/dat/webmapservices
-#mkdir -p published/dat/languages
-#mv programs/CacheWolf/* published/
-#chmod 755 published/*
-#cp lib/java_ewe.dll published/Jar/
-#chmod 644 published/*/*
-#chmod 755 published/dat/attributes
-#chmod 755 published/dat/webmapservices
-#chmod 755 published/dat/languages
-#printf '1,$g/ 12M/s///\nwq\n' | ed -s published/Jar/CacheWolf.bat
-#install -c -m 644 work/CacheWolf.ewe published/
-#install -c -m 644 res_noewe/* published/dat/
-#install -c -m 644 res_noewe/attributes-big/*.gif published/dat/attributes/
-#install -c -m 644 res_noewe/attributes/*-non.gif published/dat/attributes/
-#install -c -m 644 res_noewe/webmapservices/* published/dat/webmapservices/
-#install -c -m 644 res_noewe/languages/* published/dat/languages/
-#(
-#	cd published/dat
-#	find * -type f | sort >../flst
-#	$CPIO -oC512 -Hustar -Mdist <../flst | gzip -n9 >../datfiles.tgz
-#	zip -X ../datfiles.zip -@ <../flst
-#)
-#rm -rf published/dat published/flst
-#chmod 644 published/datfiles.tgz published/datfiles.zip
-#mkdir -p $HOME/public_html/CacheWolf-BE/r$v
-#mv published/* $HOME/public_html/CacheWolf-BE/r$v/
-#rm -rf bin published
+cd ..
+mkdir -p published/dat/attributes
+mkdir -p published/dat/webmapservices
+mkdir -p published/dat/languages
+mv deploy/CacheWolf/* published/
+chmod 755 published/*
+cp lib/java_ewe.dll published/Jar/
+chmod 644 published/*/*
+chmod 755 published/dat/attributes
+chmod 755 published/dat/webmapservices
+chmod 755 published/dat/languages
+printf '1,$g/ 12M/s///\nwq\n' | ed -s published/Jar/CacheWolf.bat
+install -c -m 644 work/CacheWolf.ewe published/
+install -c -m 644 res_noewe/* published/dat/
+install -c -m 644 res_noewe/attributes-big/*.gif published/dat/attributes/
+install -c -m 644 res_noewe/attributes/*-non.gif published/dat/attributes/
+install -c -m 644 res_noewe/webmapservices/* published/dat/webmapservices/
+install -c -m 644 res_noewe/languages/* published/dat/languages/
+(
+	cd published/dat
+	find * -type f | sort >../flst
+	$CPIO -oC512 -Hustar -Mdist <../flst | gzip -n9 >../datfiles.tgz
+	zip -X ../datfiles.zip -@ <../flst
+)
+rm -rf published/dat published/flst
+chmod 644 published/datfiles.tgz published/datfiles.zip
+mkdir -p $HOME/public_html/CacheWolf-BE/r$v
+mv published/* $HOME/public_html/CacheWolf-BE/r$v/
+rm -rf bin published
