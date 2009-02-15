e92ba5463daf92d443178e52113d4725f0be5967
pfeffer <pfeffer@55547f66-8dd2-3569-90c8-08ced114d07d>
2009-02-15 22:23:12 +0000

    make the work-around work more generally

--- experiments/EVEhist/src/eveWorkArounds/TimeWorkArounds.java
+++ experiments/EVEhist/src/eveWorkArounds/TimeWorkArounds.java
@@ -4,14 +4,18 @@ import eve.sys.Time;
 
 public class TimeWorkArounds extends Time {
 	public void parse(String dateValue,String dateFormat) throws IllegalArgumentException {
-		StringBuffer tmp = new StringBuffer(dateValue.length()+5);
-		tmp.append(dateValue.substring(0, 4)).append(" ");
-		tmp.append(dateValue.substring(4, 6)).append(" ");
-		tmp.append(dateValue.substring(6, 8)).append(" ");
-		tmp.append(dateValue.substring(8, 10)).append(" ");
-		tmp.append(dateValue.substring(10,12)).append(" ");
-		tmp.append(dateValue.substring(12, 14));
-		super.parse(tmp.toString(), dateFormat);
+		if (dateFormat.equals("yyyyMMddHHmmss")) {
+			StringBuffer tmp = new StringBuffer(dateValue.length()+5);
+			tmp.append(dateValue.substring(0, 4));
+			tmp.append(" ").append(dateValue.substring(4, 6));
+			tmp.append(" ").append(dateValue.substring(6, 8));
+			tmp.append(" ").append(dateValue.substring(8, 10));
+			tmp.append(" ").append(dateValue.substring(10,12));
+			tmp.append(" ").append(dateValue.substring(12,14));
+			super.parse(tmp.toString(), dateFormat);
+		} else {
+			super.parse(dateValue, dateFormat);
+		}
 	}
 
 }
