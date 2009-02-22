15c7fb01f1c354a1cef5b8efc4f1e233207f68d2
greiol <greiol@55547f66-8dd2-3569-90c8-08ced114d07d>
2009-02-22 09:49:32 +0000

    make the java compiler use iso-8859-1 since most of cachewolf is still encoded in windows-1252 which may not be part of the charsets supported by the jvm (see http://java.sun.com/j2se/1.4.2/docs/api/java/nio/charset/Charset.html). iso will give you much better results than the fallback to one of the utf encodings

--- compile.sh
+++ compile.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-javac -cp ./lib/CompileEwe.zip:./lib/  -d ./bin/ -deprecation -nowarn ./src/CacheWolf/*.java ./src/CacheWolf/*/*.java ./src/exp/*.java ./src/utils/*.java
+javac	-source 1.3 \
+	-target 1.1 \
+	-encoding iso-8859-1 \
+	-cp ./lib/CompileEwe.zip:./lib/  \
+	-d ./bin/ \
+	-deprecation \
+	-nowarn \
+	./src/CacheWolf/*.java \
+	./src/CacheWolf/*/*.java \
+	./src/exp/*.java \
+	./src/utils/*.java
