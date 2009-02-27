e6be721a8bff930a6404e37255fa744499c8f07a
engywuck <engywuck@55547f66-8dd2-3569-90c8-08ced114d07d>
2009-02-27 16:45:28 +0000

    Adapting to source 1.3 compliance.

--- compile.bat
+++ compile.bat
@@ -1,4 +1,4 @@
 if not exist bin\CacheWolf mkdir bin\CacheWolf
 if not exist bin\exp mkdir bin\exp
-javac -classpath ./lib/CompileEwe.zip;./lib/  -d ./bin/ -deprecation ./src/CacheWolf/*.java ./src/CacheWolf/imp/*.java ./src/CacheWolf/navi/*.java ./src/exp/*.java ./src/utils/*.java
+javac -source 1.3 -target 1.1 -classpath ./lib/CompileEwe.zip;./lib/  -d ./bin/ -deprecation ./src/CacheWolf/*.java ./src/CacheWolf/imp/*.java ./src/CacheWolf/navi/*.java ./src/exp/*.java ./src/utils/*.java
 pause
