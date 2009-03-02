cabadda4e842c7128955f90275e45162d1d8b55d
mik77 <mik77@55547f66-8dd2-3569-90c8-08ced114d07d>
2009-03-02 18:42:24 +0000

    Set new goto target to shown waypoint directly in moving map

--- src/CacheWolf/navi/MovingMap.java
+++ src/CacheWolf/navi/MovingMap.java
@@ -1472,7 +1472,7 @@ class MovingMapPanel extends InteractivePanel implements EventListener {
 	Menu kontextMenu;
 	MenuItem gotoMenuItem = new MenuItem(MyLocale.getMsg(4230, "Goto here$g"), 0, null);
 	MenuItem newWayPointMenuItem = new MenuItem(MyLocale.getMsg(4232, "Create new Waypoint here$n"), 0, null);;
-	MenuItem openCacheDescMenuItem,addCachetoListMenuItem;
+	MenuItem openCacheDescMenuItem,addCachetoListMenuItem,gotoCacheMenuItem;
 	
 	MenuItem miLuminary[];
 
@@ -1755,6 +1755,8 @@ class MovingMapPanel extends InteractivePanel implements EventListener {
 					if (clickedCache != null) {
 						openCacheDescMenuItem = new MenuItem(MyLocale.getMsg(4270, "Open")+" '"+(clickedCache.CacheName.length()>0 ? clickedCache.CacheName : clickedCache.wayPoint)+"'$o"); // clickedCache == null can happen if clicked on the goto-symbol
 						kontextMenu.addItem(openCacheDescMenuItem);
+						gotoCacheMenuItem = new MenuItem(MyLocale.getMsg(4279, "Goto")+ " '"+(clickedCache.CacheName.length()>0 ? clickedCache.CacheName : clickedCache.wayPoint)+"'$g"); // clickedCache == null can happen if clicked on the goto-symbol
+						kontextMenu.addItem(gotoCacheMenuItem);
 						if (Global.mainForm.cacheListVisible) { 
 							addCachetoListMenuItem = new MenuItem(MyLocale.getMsg(199,"Add to cachetour"));
 							kontextMenu.addItem(addCachetoListMenuItem);
@@ -1868,6 +1870,10 @@ class MovingMapPanel extends InteractivePanel implements EventListener {
 						mm.postEvent(close);
 						MainTab mainT = Global.mainTab;
 						mainT.openDescriptionPanel(clickedCache);
+					}
+					if (action == gotoCacheMenuItem) {
+						kontextMenu.close();
+						mm.myNavigation.setDestination(clickedCache.pos);	
 					}
 					if (action == newWayPointMenuItem) {
 						kontextMenu.close();
@@ -2209,5 +2215,7 @@ class ArrowsOnMap extends AniImage {
 		g.drawLine(arrow[0].x, arrow[0].y, arrow[1].x,arrow[1].y);
 	}
 }
+
+
 
 
