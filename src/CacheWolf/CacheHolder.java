c0dba3b44a78e17b677172399a757559e7e09fd2
engywuck <engywuck@55547f66-8dd2-3569-90c8-08ced114d07d>
2009-03-26 16:15:18 +0000

    Changed data type of cache type from string to int. (Applied changes already done in Rev. 1318/1319)

--- src/CacheWolf/CacheHolder.java
+++ src/CacheWolf/CacheHolder.java
@@ -55,7 +55,7 @@ public class CacheHolder {
 	/** The terrain rating of the cache from 1 to 5 in .5 incements */
 	public String terrain = EMPTY;
 	/** The cache type (@see CacheType for translation table)  */
-	public String type = "0"; //TODO Should be an int
+	public int type = 0; 
 	/** True if the cache has been archived */
 	public boolean is_archived = false;
 	/** True if the cache is available for searching */
@@ -157,7 +157,7 @@ public class CacheHolder {
 			CacheStatus = xmlString.substring(start+1,end);
 
 			start=xmlString.indexOf('"',end+1); end=xmlString.indexOf('"',start+1);
-			type = xmlString.substring(start+1,end);
+			type = new Integer(xmlString.substring(start+1,end)).intValue();
 
 			start=xmlString.indexOf('"',end+1); end=xmlString.indexOf('"',start+1);
 			hard = xmlString.substring(start+1,end);
--- src/CacheWolf/Parser.java
+++ src/CacheWolf/Parser.java
@@ -689,10 +689,10 @@ public class Parser{
 				String stage=MyLocale.formatLong(i,"00");
 				String stageWpt="$"+stage+waypointName.substring(2);
 				String stageName = "Stage "+(i+1);
-				String type = "51";
+				int type = 51;
 				if (i == nStages - 1) {
 					stageName = "Final";
-					type = "53";
+					type = 53;
 				}
 				didCreateWp|=createWptIfNeeded(stage+waypointName.substring(2), stageName, type);
 				op.append("IF "+stageWpt+"=\"\" THEN\n");
@@ -1096,7 +1096,7 @@ public class Parser{
 		}
 	}
 
-	private boolean createWptIfNeeded(String wayPoint, String name, String type){
+	private boolean createWptIfNeeded(String wayPoint, String name, int type){
 	   	int ci=Global.getProfile().getCacheIndex(wayPoint);
     	if (ci >= 0) return false;
 
