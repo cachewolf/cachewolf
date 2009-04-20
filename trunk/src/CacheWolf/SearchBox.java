9da2b0908414c69e333be116149909e2074c670f
engywuck <engywuck@55547f66-8dd2-3569-90c8-08ced114d07d>
2009-04-20 17:27:00 +0000

    Added possibility to search in Description and Notes of cache.

---
+++ trunk/src/CacheWolf/SearchBox.java
@@ -0,0 +1,95 @@
+package CacheWolf;
+
+import ewe.ui.CellConstants;
+import ewe.ui.Control;
+import ewe.ui.Frame;
+import ewe.ui.InputBox;
+import ewe.ui.mCheckBox;
+
+/**
+ * A SearchBox is a customized input box optimized for searching in CacheWolf. The actual
+ * implementation is able to display a CheckBox which with label "in Notes/Description".
+
+ * @author Engywuck
+ */
+public class SearchBox extends InputBox {
+
+	protected mCheckBox useNoteDesc;
+	protected boolean buildingForm = false;
+
+	/**
+	 * Creates the search box with given title.
+	 * @param title The titel of the box
+	 */
+	public SearchBox(String title) {
+		super(title);
+	}
+
+	/**
+	 * Displays the search Box and returns the String value entered, if OK is pressed, otherwise
+	 * the value is null.
+	 * @param initialValue Initial value to display in the search box
+	 * @param checkUseNoteDesc Initial value for check box
+	 * @param pWidth ?
+	 * @return String to search for if ok is pressed and a string is entered, <code>null</code> otherwise.
+	 */
+	public String input(String initialValue, boolean checkUseNoteDesc, int pWidth) {
+		return this.input(null, initialValue, checkUseNoteDesc, pWidth);
+	}
+	
+	protected String input(Frame pParent, String initialValue, boolean checkUseNoteDesc, int pWidth) {
+		String result;
+		buildingForm = true;
+		useNoteDesc = new mCheckBox(MyLocale.getMsg(218,"Also in description/notes"));//,CellConstants.DONTSTRETCH, (CellConstants.DONTFILL|CellConstants.WEST));
+		useNoteDesc.setState(checkUseNoteDesc);		
+		result = super.input(pParent, initialValue, pWidth);
+		return result;
+    }
+	
+	/**
+	 * Queries the check box if it is checked or not.
+	 * @return <code>True</code> if check box is checked, <code>false</code> if not.
+	 */
+	public boolean useNoteDesc() {
+		boolean result = false;
+		if (useNoteDesc != null) {
+			result = useNoteDesc.getState();
+		}
+		return result;
+	}
+	
+	public Control addLast(Control c) {
+		// This method is a dirty hack, because in InputBox every thing, from creation of the 
+		// controls to displaying it and returning the return value is done at once.
+		// To be able to add other controls, I have to enhance the addLast(Control) method - not
+		// nice but it works.
+		Control result;
+		if (!buildingForm) {
+			result = super.addLast(c);
+		} else {
+			buildingForm = false;
+			this.addControlsBeforeInput();
+			result = super.addLast(c);
+			this.addControlsAfterInput();
+		}
+		return result;
+	}
+
+	/**
+	 * Called before creating the input box. Additional controls may be added here.
+	 */
+	private void addControlsBeforeInput() {
+		// For future use 
+	}
+
+	/**
+	 * Called after creating the input box. Additional controls may be added here.
+	 */
+	private void addControlsAfterInput() {
+		if (useNoteDesc != null) {
+			this.addLast(useNoteDesc,CellConstants.DONTSTRETCH, (CellConstants.DONTFILL|CellConstants.WEST));
+		}
+    }
+
+
+}
