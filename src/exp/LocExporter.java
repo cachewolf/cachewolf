82c3658cefa8ed53925490fee7b163d049474f20
salzkammergut <salzkammergut@55547f66-8dd2-3569-90c8-08ced114d07d>
2009-05-30 19:31:51 +0000

    Bugfix: LocExporter. Corrected evaluation precedence of boolean expression

--- src/exp/LocExporter.java
+++ src/exp/LocExporter.java
@@ -126,12 +126,12 @@ public class LocExporter extends Exporter{
 				IconMap icon=(IconMap) symbols.get(i);
 				boolean match=true;
 				// If a certainattribute is not null it must match the current caches values
-				match=match && (icon.type==null) || ch.getType()==0 || icon.type.equals(String.valueOf(ch.getType()));
-				match=match && (icon.size==null) || ch.getCacheSize()==0 || icon.size.equalsIgnoreCase(CacheSize.getExportShortId(ch.getCacheSize()));
-				match=match && (icon.terrain==null) || ch.getTerrain()==0 || icon.terrain.equals(CacheTerrDiff.shortDT(ch.getTerrain()));
-				match=match && (icon.difficulty==null) ||  ch.getHard()==0 || icon.difficulty.equals(CacheTerrDiff.shortDT(ch.getHard()));
-				match=match && (icon.status==null) ||  ch.getCacheStatus().startsWith(icon.status);
-				match=match && (icon.found==null) || ch.is_found();
+				match=match && ((icon.type==null) || ch.getType()==0 || icon.type.equals(String.valueOf(ch.getType())));
+				match=match && ((icon.size==null) || ch.getCacheSize()==0 || icon.size.equalsIgnoreCase(CacheSize.getExportShortId(ch.getCacheSize())));
+				match=match && ((icon.terrain==null) || ch.getTerrain()==0 || icon.terrain.equals(CacheTerrDiff.shortDT(ch.getTerrain())));
+				match=match && ((icon.difficulty==null) ||  ch.getHard()==0 || icon.difficulty.equals(CacheTerrDiff.shortDT(ch.getHard())));
+				match=match && ((icon.status==null) ||  ch.getCacheStatus().startsWith(icon.status));
+				match=match && ((icon.found==null) || ch.is_found());
 				if (match) return icon.name;
 			}
 
