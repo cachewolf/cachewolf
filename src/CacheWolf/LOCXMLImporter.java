98b511424a8c07a211b98ee6d30590fc10fa7f5d
greiol <greiol@55547f66-8dd2-3569-90c8-08ced114d07d>
2009-05-30 20:35:51 +0000

    adapted LOC impoeter to new types ran eclipse formatter on LOC importer

--- src/CacheWolf/LOCXMLImporter.java
+++ src/CacheWolf/LOCXMLImporter.java
@@ -1,30 +1,32 @@
-    /*
-    CacheWolf is a software for PocketPC, Win and Linux that 
-    enables paperless caching. 
-    It supports the sites geocaching.com and opencaching.de
-    
-    Copyright (C) 2006  CacheWolf development team
-    See http://developer.berlios.de/projects/cachewolf/
-    for more information.
-    Contact: 	bilbowolf@users.berlios.de
-    			kalli@users.berlios.de
+/*
+CacheWolf is a software for PocketPC, Win and Linux that 
+enables paperless caching. 
+It supports the sites geocaching.com and opencaching.de
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+Copyright (C) 2006  CacheWolf development team
+See http://developer.berlios.de/projects/cachewolf/
+for more information.
+Contact: 	bilbowolf@users.berlios.de
+			kalli@users.berlios.de
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-    */
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 package CacheWolf;
+
+//TODO: move to CacheWOlf.imp
 
 import ewe.sys.Vm;
 import ewe.io.*;
@@ -33,7 +35,7 @@ import ewesoft.xml.sax.AttributeList;
 
 /**
  * @author Kalle
- *
+ * 
  */
 public class LOCXMLImporter extends MinML {
 	boolean debugXML = false;
@@ -45,61 +47,59 @@ public class LOCXMLImporter extends MinML {
 
 	String strData = new String();
 
-	
-	public LOCXMLImporter ( Preferences pf, Profile prof, String f ){
+	public LOCXMLImporter(Preferences pf, Profile prof, String f) {
 		pref = pf;
-		profile=prof;
+		profile = prof;
 		cacheDB = profile.cacheDB;
 		file = f;
 	}
-	
+
 	public void doIt() {
-		try{
+		try {
 			Reader r;
 			Vm.showWait(true);
-			//Test for zip.file
-						r = new FileReader(file);
-						parse(r);
-						r.close();
-			// save Index 
-			profile.saveIndex(pref,Profile.NO_SHOW_PROGRESS_BAR);
+			// Test for zip.file
+			r = new FileReader(file);
+			parse(r);
+			r.close();
+			// save Index
+			profile.saveIndex(pref, Profile.NO_SHOW_PROGRESS_BAR);
 			Vm.showWait(false);
-		}catch(Exception e){
-			//Vm.debug(e.toString());
+		} catch (Exception e) {
+			// Vm.debug(e.toString());
 			Vm.showWait(false);
 		}
-		
 	}
-	
-	public void startElement(String name, AttributeList atts){
-		if (debugXML){
+
+	public void startElement(String name, AttributeList atts) {
+		if (debugXML) {
 			for (int i = 0; i < atts.getLength(); i++) {
-				Vm.debug(" Name: " + atts.getName(i)+ " Value: "+atts.getValue(i));
+				Vm.debug(" Name: " + atts.getName(i) + " Value: "
+						+ atts.getValue(i));
 			}
 		}
-		strData ="";
-		if (name.equals("name")){
+		strData = "";
+		if (name.equals("name")) {
 			holder = getHolder(atts.getValue("id"));
 			return;
 		}
-		if (name.equals("coord")){
-			holder.pos.set(Common.parseDouble(atts.getValue("lat")),Common.parseDouble(atts.getValue("lon")));
+		if (name.equals("coord")) {
+			holder.pos.set(Common.parseDouble(atts.getValue("lat")), Common
+					.parseDouble(atts.getValue("lon")));
 			holder.LatLon = holder.pos.toString();
 			return;
 		}
-
-		
 	}
-	
-	public void endElement(String name){
-		if (name.equals("name")){
+
+	public void endElement(String name) {
+		if (name.equals("name")) {
 			holder.setCacheName(strData);
 		}
 
-		if (name.equals("waypoint")){
+		if (name.equals("waypoint")) {
 			int index;
 			index = cacheDB.getIndex(holder.getWayPoint());
-			if (index == -1){
+			if (index == -1) {
 				holder.setNew(true);
 				cacheDB.add(holder);
 			}
@@ -107,38 +107,40 @@ public class LOCXMLImporter extends MinML {
 			else {
 				holder.setNew(false);
 			}
-			// save all  (after each cache???)
+			// save all (after each cache???)
 			holder.save();
-			profile.saveIndex(pref,Profile.NO_SHOW_PROGRESS_BAR);
+			profile.saveIndex(pref, Profile.NO_SHOW_PROGRESS_BAR);
 			return;
 		}
 
-		if (name.equals("link")){
+		if (name.equals("link")) {
 			holder.getFreshDetails().URL = strData;
 			return;
 		}
-
-
-
 	}
-	
 
-	public void characters(char[] ch,int start,int length){
-		String chars = new String(ch,start,length);
+	public void characters(char[] ch, int start, int length) {
+		String chars = new String(ch, start, length);
 		strData += chars;
-		if (debugXML) Vm.debug(strData);
+		if (debugXML)
+			Vm.debug(strData);
 	}
 
-	private CacheHolder getHolder(String wpt){// See also OCXMLImporter
+	private CacheHolder getHolder(String wpt) {// See also OCXMLImporter
 		CacheHolder ch;
-		
+
 		ch = cacheDB.get(wpt);
 		if (ch == null) {
 			ch = new CacheHolder();
+			// TODO: once the cleanup is done, this should be move to
+			// CacheHolder constructor
+			ch.setType(CacheType.CW_TYPE_CUSTOM);
+			ch.setTerrain(CacheTerrDiff.CW_DT_UNSET);
+			ch.setHard(CacheTerrDiff.CW_DT_UNSET);
+			ch.setCacheSize(CacheSize.CW_SIZE_NOTCHOSEN);
 			ch.setWayPoint(wpt);
 		}
 		return ch;
 	}
-
 
 }
