4232677ba1864b36871db11424a11e19049f6ef1
greiol <greiol@55547f66-8dd2-3569-90c8-08ced114d07d>
2009-08-19 20:09:23 +0000

    - reduced visibility of properties and parameters - added curly braces to if statements - replaced string appending with string buffers - added calls to super() in internal classes - removed setNeedsTabelUpdate()

--- trunk/src/CacheWolf/GuiImageBroker.java
+++ trunk/src/CacheWolf/GuiImageBroker.java
@@ -16,13 +16,16 @@ public final class GuiImageBroker {
 
 	/** image to be displayed in case of error */
 	public static Image imageError = new Image("guiError.png");
+	
+	/** reference to this object */
+	private static GuiImageBroker ref;
 
 	/**
 	 * images to be displayed for cache types in GUI
 	 * @see getTypeImage
 	 * @see CacheTypes
 	 */
-	private static final Image[] typeImages = {
+	private static final Image[] TYPEIMAGES = {
 		new Image(CacheType.CW_GUIIMG_CUSTOM),		// 0
 		new Image(CacheType.CW_GUIIMG_APE),			// 1
 		new Image(CacheType.CW_GUIIMG_CITO),		// 2
@@ -48,48 +51,58 @@ public final class GuiImageBroker {
 	};
 
 	// TODO: move size images here
-	private static final Image[] sizeImages = {
+//	private static final Image[] sizeImages = {
 //		new Image(CacheSize.CW_GUIIMG_NONPHYSICAL),
 //		new Image(CacheSize.CW_GUIIMG_MICRO),
 //		new Image(CacheSize.CW_GUIIMG_SMALL),
 //		new Image(CacheSize.CW_GUIIMG_NORMAL),
 //		new Image(CacheSize.CW_GUIIMG_LARGE),
 //		new Image(CacheSize.CW_GUIIMG_VERYLARGE)
-	};
+//	};
 
-	/** constructor does nothing */
-	private GuiImageBroker() { // no instantiation needed
+	/** thou shallst not instantiate this object */
+	private GuiImageBroker() { }
+	
+	/**
+	 * give a singleton reference to this object to whoever needs one. create one, if not done so already.
+	 * @return reference to CWWrapper
+	 */
+	public static synchronized GuiImageBroker getGuiImageBroker() {
+		if (ref == null) {
+			ref = new GuiImageBroker();
+		}
+		return ref;
 	}
 
 	/**
 	 * select image to be displayed for a given cache type
-	 * @param id internal cache type id
+	 * @param type internal cache type id
 	 * @return <code>Image</code> object to be displayed
 	 */
-	public static Image getTypeImage(byte id) {
-		switch (id) {
-		case CacheType.CW_TYPE_CUSTOM: return typeImages[0];
-		case CacheType.CW_TYPE_APE: return typeImages[1];
-		case CacheType.CW_TYPE_CITO: return typeImages[2];
-		case CacheType.CW_TYPE_DRIVE_IN: return typeImages[3];
-		case CacheType.CW_TYPE_EARTH: return typeImages[4];
-		case CacheType.CW_TYPE_EVENT: return typeImages[5];
-		case CacheType.CW_TYPE_FINAL: return typeImages[6];
-		case CacheType.CW_TYPE_LETTERBOX: return typeImages[7];
-		case CacheType.CW_TYPE_LOCATIONLESS: return typeImages[8];
-		case CacheType.CW_TYPE_MAZE: return typeImages[9];
-		case CacheType.CW_TYPE_MEGA_EVENT: return typeImages[10];
-		case CacheType.CW_TYPE_MULTI: return typeImages[11];
-		case CacheType.CW_TYPE_PARKING: return typeImages[12];
-		case CacheType.CW_TYPE_QUESTION: return typeImages[13];
-		case CacheType.CW_TYPE_REFERENCE: return typeImages[14];
-		case CacheType.CW_TYPE_STAGE: return typeImages[15];
-		case CacheType.CW_TYPE_TRADITIONAL: return typeImages[16];
-		case CacheType.CW_TYPE_TRAILHEAD: return typeImages[17];
-		case CacheType.CW_TYPE_UNKNOWN: return typeImages[18];
-		case CacheType.CW_TYPE_VIRTUAL: return typeImages[19];
-		case CacheType.CW_TYPE_WEBCAM: return typeImages[20];
-		case CacheType.CW_TYPE_WHEREIGO: return typeImages[21];
+	public static Image getTypeImage(final byte type) {
+		switch (type) {
+		case CacheType.CW_TYPE_CUSTOM: return TYPEIMAGES[0];
+		case CacheType.CW_TYPE_APE: return TYPEIMAGES[1];
+		case CacheType.CW_TYPE_CITO: return TYPEIMAGES[2];
+		case CacheType.CW_TYPE_DRIVE_IN: return TYPEIMAGES[3];
+		case CacheType.CW_TYPE_EARTH: return TYPEIMAGES[4];
+		case CacheType.CW_TYPE_EVENT: return TYPEIMAGES[5];
+		case CacheType.CW_TYPE_FINAL: return TYPEIMAGES[6];
+		case CacheType.CW_TYPE_LETTERBOX: return TYPEIMAGES[7];
+		case CacheType.CW_TYPE_LOCATIONLESS: return TYPEIMAGES[8];
+		case CacheType.CW_TYPE_MAZE: return TYPEIMAGES[9];
+		case CacheType.CW_TYPE_MEGA_EVENT: return TYPEIMAGES[10];
+		case CacheType.CW_TYPE_MULTI: return TYPEIMAGES[11];
+		case CacheType.CW_TYPE_PARKING: return TYPEIMAGES[12];
+		case CacheType.CW_TYPE_QUESTION: return TYPEIMAGES[13];
+		case CacheType.CW_TYPE_REFERENCE: return TYPEIMAGES[14];
+		case CacheType.CW_TYPE_STAGE: return TYPEIMAGES[15];
+		case CacheType.CW_TYPE_TRADITIONAL: return TYPEIMAGES[16];
+		case CacheType.CW_TYPE_TRAILHEAD: return TYPEIMAGES[17];
+		case CacheType.CW_TYPE_UNKNOWN: return TYPEIMAGES[18];
+		case CacheType.CW_TYPE_VIRTUAL: return TYPEIMAGES[19];
+		case CacheType.CW_TYPE_WEBCAM: return TYPEIMAGES[20];
+		case CacheType.CW_TYPE_WHEREIGO: return TYPEIMAGES[21];
 		default: return imageError;
 		}
 	}
@@ -103,24 +116,23 @@ public final class GuiImageBroker {
 	 * replaced by the image in x.png.
 	 * Images are NOT checked for size etc.
 	 */
-	public static void customizedSymbols()
-	{
-		FileBugfix dir=new FileBugfix(FileBase.getProgramDirectory()+"/symbols");
+	public static void customizedSymbols() {
+		final FileBugfix dir = new FileBugfix(FileBase.getProgramDirectory()+"/symbols");
 		if (dir.isDirectory()){
-			int id;
+			int type;
 			String name = "";
 			String [] pngFiles;
 			pngFiles=dir.list("*.png",0);
-			for (int i=0; i<pngFiles.length; i++) {
+			for (int i=0; i < pngFiles.length; i++) {
 				name = pngFiles[i].substring(0,pngFiles[i].length()-4);
 				try {
-					id = Integer.parseInt (name);
+					type = Integer.parseInt(name);
 				}
 				catch (Exception E){
-					id = -1; //filename invalid for symbols
+					type = -1; //filename invalid for symbols
 				}
-				if (0<=id && id<=typeImages.length){
-					typeImages[id]= new Image(FileBase.getProgramDirectory()+"/symbols/"+pngFiles[i]);
+				if (0<=type && type<=TYPEIMAGES.length){
+					TYPEIMAGES[type] = new Image(FileBase.getProgramDirectory()+"/symbols/"+pngFiles[i]);
 				}
 			}
 		}
