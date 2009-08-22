41b696d2e66cc0695289cb38625e7f63dc543214
engywuck <engywuck@55547f66-8dd2-3569-90c8-08ced114d07d>
2009-08-22 11:06:00 +0000

    Removing unwanted/unneeded class; removing interface from MutableInteger, as it is not needed and may slow down usage.

--- trunk/src/CacheWolf/utils/MutableInteger.java
+++ trunk/src/CacheWolf/utils/MutableInteger.java
@@ -1,27 +1,40 @@
 package CacheWolf.utils;
 
-import ewe.util.Intable;
-
 /**
  * Mutable Integer class, in case we need several integer Values
  * which could be reused in order to decrease object creation. 
  */
-public class MutableInteger implements Intable {
+public class MutableInteger {
 
 	private int value;
 	
+	/**
+	 * Creates a MutableInteger Object with value -1
+	 */
 	public MutableInteger() {
 		value = -1;
 	}
 	
+	/**
+	 * Creates a MutableInteger Object with specified value
+	 * @param initialValue You have to guess.
+	 */
 	public MutableInteger(final int initialValue) {
 		value = initialValue;
 	}
 
+	/**
+	 * Gets the value
+	 * @return value
+	 */
 	public int getInt(){
 		return value;
 	}
 	
+	/**
+	 * Sets the value
+	 * @param newValue value
+	 */
 	public void setInt(final int newValue) {
 		value = newValue;
 	}
