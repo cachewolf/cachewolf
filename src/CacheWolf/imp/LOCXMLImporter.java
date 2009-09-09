3d1cc5eaa0c0bd4f65fc6f10db275f3345b85ca0
pfefferCode <prog@robert-arnold.de>
2009-09-09 11:28:05 +0000

    opencaching remove ".de" was not complete opencaching download: bug fixed: <waypoint>.xml were not created at first download. In order to solve this problem, Cacheholder.CachesWithLoadedDetails now again holds a vector of the Cacheholders directly, not a list of waypoint names, see http://www.geoclub.de/viewtopic.php?f=40&t=33289&start=20 for details

--- src/CacheWolf/imp/LOCXMLImporter.java
+++ src/CacheWolf/imp/LOCXMLImporter.java
@@ -26,7 +26,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 package CacheWolf.imp;
 
-//TODO: move to CacheWOlf.imp
 
 import CacheWolf.CacheDB;
 import CacheWolf.CacheHolder;
