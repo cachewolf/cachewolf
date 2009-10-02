9941ca6b21ecf6ae37a95fe938c16a966f0f29fe
pfefferCode <prog@robert-arnold.de>
2009-10-02 18:35:18 +0000

    MakeWmsTool: added feature to finde optimal scale

--- tools/MakeWMSFile/capxml2wms/Map.Designer.vb
+++ tools/MakeWMSFile/capxml2wms/Map.Designer.vb
@@ -23,6 +23,9 @@ Partial Class Map
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.PictureBox = New System.Windows.Forms.PictureBox
+        Me.LabelScaleVertical = New System.Windows.Forms.Label
+        Me.ButtonScaleDown = New System.Windows.Forms.Button
+        Me.ButtonScaleUp = New System.Windows.Forms.Button
         CType(Me.PictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -31,23 +34,57 @@ Partial Class Map
         Me.PictureBox.Dock = System.Windows.Forms.DockStyle.Fill
         Me.PictureBox.Location = New System.Drawing.Point(0, 0)
         Me.PictureBox.Name = "PictureBox"
-        Me.PictureBox.Size = New System.Drawing.Size(451, 361)
+        Me.PictureBox.Size = New System.Drawing.Size(548, 505)
         Me.PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.PictureBox.TabIndex = 0
         Me.PictureBox.TabStop = False
+        '
+        'LabelScaleVertical
+        '
+        Me.LabelScaleVertical.AutoSize = True
+        Me.LabelScaleVertical.Location = New System.Drawing.Point(224, 470)
+        Me.LabelScaleVertical.Name = "LabelScaleVertical"
+        Me.LabelScaleVertical.Size = New System.Drawing.Size(39, 13)
+        Me.LabelScaleVertical.TabIndex = 1
+        Me.LabelScaleVertical.Text = "Label1"
+        '
+        'ButtonScaleDown
+        '
+        Me.ButtonScaleDown.Location = New System.Drawing.Point(361, 470)
+        Me.ButtonScaleDown.Name = "ButtonScaleDown"
+        Me.ButtonScaleDown.Size = New System.Drawing.Size(85, 23)
+        Me.ButtonScaleDown.TabIndex = 2
+        Me.ButtonScaleDown.Text = "Scale down"
+        Me.ButtonScaleDown.UseVisualStyleBackColor = True
+        '
+        'ButtonScaleUp
+        '
+        Me.ButtonScaleUp.Location = New System.Drawing.Point(73, 470)
+        Me.ButtonScaleUp.Name = "ButtonScaleUp"
+        Me.ButtonScaleUp.Size = New System.Drawing.Size(85, 23)
+        Me.ButtonScaleUp.TabIndex = 3
+        Me.ButtonScaleUp.Text = "Scale up"
+        Me.ButtonScaleUp.UseVisualStyleBackColor = True
         '
         'Map
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(451, 361)
+        Me.ClientSize = New System.Drawing.Size(548, 505)
+        Me.Controls.Add(Me.ButtonScaleUp)
+        Me.Controls.Add(Me.ButtonScaleDown)
+        Me.Controls.Add(Me.LabelScaleVertical)
         Me.Controls.Add(Me.PictureBox)
         Me.Name = "Map"
         Me.ShowIcon = False
         Me.Text = "Map"
         CType(Me.PictureBox, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Private WithEvents PictureBox As System.Windows.Forms.PictureBox
+    Friend WithEvents LabelScaleVertical As System.Windows.Forms.Label
+    Friend WithEvents ButtonScaleDown As System.Windows.Forms.Button
+    Friend WithEvents ButtonScaleUp As System.Windows.Forms.Button
 End Class
--- tools/MakeWMSFile/capxml2wms/Map.vb
+++ tools/MakeWMSFile/capxml2wms/Map.vb
@@ -3,6 +3,10 @@ Public Class Map
     Dim GetMapUrl As String
     Dim pngFileName As String
     Dim i As Image
+    Dim scale_ As Double
+    Public exitcode As Integer
+    Public clickedAt As Point
+
 
     Private Sub Map_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
         Try
@@ -31,13 +35,36 @@ Public Class Map
         End Try
     End Sub
 
-    Public Sub New(ByVal _GetMapUrl As String)
+    Public Sub New(ByVal _GetMapUrl As String, ByVal scale As Double)
 
         ' Dieser Aufruf ist für den Windows Form-Designer erforderlich.
         InitializeComponent()
 
         ' Fügen Sie Initialisierungen nach dem InitializeComponent()-Aufruf hinzu.
         GetMapUrl = _GetMapUrl
+        scale_ = scale
+        LabelScaleVertical.Text = "Vertical scale m/pixel: " & scale
+    End Sub
 
+    Private Sub ButtonScaleUp_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonScaleUp.Click
+        exitcode = +1
+        Close()
+    End Sub
+
+    Private Sub ButtonScaleDown_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonScaleDown.Click
+        exitcode = -1
+        Close()
+    End Sub
+
+    Private Sub PictureBox_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox.Click
+        Dim mev As Windows.Forms.MouseEventArgs
+        mev = e
+        Dim im As RectangleF
+        im = PictureBox.Image.GetBounds(System.Drawing.GraphicsUnit.Pixel)
+        clickedAt = New Point
+        clickedAt.X = mev.X - (Width - im.Width) / 2 - im.Width / 2
+        clickedAt.Y = mev.Y - (Height - im.Height) / 2 - im.Height / 2
+        exitcode = 5
+        Close()
     End Sub
 End Class
