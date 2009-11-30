26e67a8a19cafd8e00bc37ca06a03bfd47d5ff94
araber95 <araber95@55547f66-8dd2-3569-90c8-08ced114d07d>
2009-11-30 18:12:59 +0000

    Startfiles for CW , prefering java on windows

--- res_noewe/cachewolf.bat
+++ res_noewe/cachewolf.bat
@@ -7,7 +7,7 @@ rem otherwise use cachewolf.exe (= cachewolf.ewe with integrated ewe-vm)
 if exist CacheWolf.jar (
 javaw -version 2>nul
 if not errorlevel 1 (
-start javaw -cp CacheWolf.jar ewe.applet.Applet CacheWolf.CacheWolf
+start javaw -Xms64M -Xmx1024M -cp CacheWolf.jar ewe.applet.Applet CacheWolf.CacheWolf
 ) else (
 start cachewolf.exe
 ) ) else (
