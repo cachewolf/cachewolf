c6c64d53fe89ebd60806dff13b68158462f20d25
araber95 <araber95@55547f66-8dd2-3569-90c8-08ced114d07d>
2009-12-14 19:29:58 +0000

    exp had been moved long time ago ( no longer needed)

--- trunk/compile.bat
+++ trunk/compile.bat
@@ -1,4 +1,3 @@
 if not exist bin\CacheWolf mkdir bin\CacheWolf
-if not exist bin\exp mkdir bin\exp
 javac -source 1.3 -target 1.1 -classpath ./lib/CompileEwe.zip;./lib/  -d ./bin/ -deprecation ./src/CacheWolf/*.java ./src/CacheWolf/imp/*.java ./src/CacheWolf/navi/*.java ./src/CacheWolf/exp/*.java ./src/CacheWolf/utils/*.java
 pause
