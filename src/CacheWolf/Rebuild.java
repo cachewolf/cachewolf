718c96225b41c8694a0910dc463977ae0957829d
Arbor95 <arbor95@gmx.de>
2009-12-22 10:04:03 +0000

    deleting caches not in profile is no longer necessary like before (alternative move to a new profile to see remaining files)

--- src/CacheWolf/Rebuild.java
+++ src/CacheWolf/Rebuild.java
@@ -9,7 +9,6 @@ import ewe.ui.MessageBox;
 import ewe.ui.ProgressBarForm;
 
 public class Rebuild {
-	String [] xmlFiles;
 	private int cacheXmlVersion;
 	private int startOfCacheDetails;
 
@@ -22,80 +21,67 @@ public class Rebuild {
 
 		myProgressBarForm pbf = new myProgressBarForm();
 		Handle h = new Handle();
-		pbf.setTask(h,MyLocale.getMsg(209,"Rebuilding index"));
-		pbf.exec();
 
-		FileBugfix file=new FileBugfix(Global.getProfile().dataDir);
-		xmlFiles=file.list("*.xml",0);
 		int orphans=0; // xml Files without entry in database : Changed to all entries!!!
 		int nAdded=0;  // caches added to database
-		for (i=0; i<xmlFiles.length; i++) {
-			int pos=xmlFiles[i].lastIndexOf('.');
-			if (pos<0) continue;
-			String wayPoint=xmlFiles[i].substring(0,pos).toUpperCase();
-			if (wayPoint.equalsIgnoreCase("index"))	// Check for index.xml and index.bak
-				xmlFiles[i]=null;   				// Remove (existing caches) or index.xml
-			else {
-				//ewe.sys.Vm.debug("Orphan: "+wayPoint);
-				orphans++;
-				int cacheIndex=prof.getCacheIndex(wayPoint);
-				if (cacheIndex > -1) {
-					// In older Versions of CW the <CACHE... /> line was not stored in the cache.xml
-					// therefore get it from the index.xml (prof.cacheDB) and put it into the cache.xml
-					getCacheDetails(prof.dataDir+xmlFiles[i]);
-					if (startOfCacheDetails < 0) {
-						CacheHolder ch=prof.cacheDB.get(cacheIndex);
-						ch.save();
+		boolean checkOlder;
+		MessageBox mBox = new MessageBox("", MyLocale.getMsg(5522,"Check for older Versions xml-Files ( without <CACHE... /> )") , FormBase.IDYES |FormBase.IDNO);
+		if (mBox.execute() == FormBase.IDOK){checkOlder=true;}
+		else {checkOlder=false;}
+		// still time consuming the list files
+		pbf.setTask(h, "Reading Directory, be patient");
+		pbf.exec();
+		h.progress=(float) 0.5;
+		h.changed();
+		String[] CacheFiles = new FileBugfix(prof.dataDir).list("*.xml", FileBase.LIST_FILES_ONLY|FileBase.LIST_DONT_SORT);
+		pbf.setTask(h,"checking XML-files");
+		for (i=0; i<CacheFiles.length; i++) {
+				int pos=CacheFiles[i].lastIndexOf('.');
+				if (pos<0) continue;
+				String wayPoint=CacheFiles[i].substring(0,pos).toUpperCase();
+				if (wayPoint.equalsIgnoreCase("index"))	// Check for index.xml and index.bak
+					CacheFiles[i]=null;   				// Remove (existing caches) or index.xml
+				else {
+					//ewe.sys.Vm.debug("Orphan: "+wayPoint);
+					orphans++;
+					h.progress = ((float)(orphans)/(float)CacheFiles.length);
+					h.changed();
+					if (checkOlder) {
+						int cacheIndex=prof.getCacheIndex(wayPoint);
+						if (cacheIndex > -1) {
+							// In older Versions of CW the <CACHE... /> line was not stored in the cache.xml
+							// therefore get it from the index.xml (prof.cacheDB) and put it into the cache.xml
+							getCacheDetails(prof.dataDir+CacheFiles[i]);
+							if (startOfCacheDetails < 0) {
+								CacheHolder ch=prof.cacheDB.get(cacheIndex);
+								ch.save();
+							}
+							// prof.cacheDB.removeElementAt(cacheIndex);
+						}
 					}
-					// prof.cacheDB.removeElementAt(cacheIndex);
 				}
-			}
 		}
 		prof.cacheDB.clear(); //easier than removeElementAt
+		pbf.setTask(h,MyLocale.getMsg(209,"Rebuilding index"));
 		if (orphans>0) { // At least one cache not in database
 			int nProcessed=0;
 			// Now do the actual work
-			for(i = 0; i<xmlFiles.length; i++){
-				if (xmlFiles[i]!=null) {
+			for(i = 0; i<CacheFiles.length; i++){
+				if (CacheFiles[i]!=null) {
 					h.progress = ((float)nProcessed++)/(float)(orphans);
 					h.changed();
-					String details=getCacheDetails(prof.dataDir+xmlFiles[i]);
+					String details=getCacheDetails(prof.dataDir+CacheFiles[i]);
 					if (details!=null) {
 						CacheHolder ch=new CacheHolder(details, cacheXmlVersion);
 						prof.cacheDB.add(ch);
 						nAdded++;
-						xmlFiles[i]=null;
-					} else Global.getPref().log("File "+xmlFiles[i]+" not in index.xml");
+						CacheFiles[i]=null;
+					} else Global.getPref().log("File "+CacheFiles[i]+" not in index.xml");
 				}
 				if (pbf.isClosed) break;
 			}
-			(new MessageBox(MyLocale.getMsg(327, "Information"),
-					  MyLocale.getMsg(210,"Caches deleted: ")+orphans+
-					  MyLocale.getMsg(211,"\nCaches added: ")+nAdded
-					, FormBase.OKB)).execute();
 			prof.buildReferences();
 			prof.saveIndex(Global.getPref(),true);
-		}
-		if (orphans!=nAdded && (new MessageBox(MyLocale.getMsg(327, "Information"),
-					MyLocale.getMsg(212,"Delete all .xml files not in index.xml and associated pictures"),
-					FormBase.YESB | FormBase.NOB)).execute()==FormBase.YESB) {
-			h = new Handle();
-			pbf.setTask(h,MyLocale.getMsg(213,"Deleting orphans"));
-			DataMover dm=new DataMover();
-			int nDeleted=0;
-			String[] CacheFiles = new FileBugfix(prof.dataDir).list(null, FileBase.LIST_FILES_ONLY|FileBase.LIST_DONT_SORT);
-			for (i=0; i<xmlFiles.length; i++) {
-				if (xmlFiles[i]!=null){
-					h.progress = ((float)nDeleted++)/(float)(orphans-nAdded);
-					h.changed();
-					int dotPos = xmlFiles[i].indexOf('.');
-					if (dotPos > 0) {
-					    // This may appear when there are directories in the profile
-					    String wayPoint=xmlFiles[i].substring(0,dotPos);
-					    dm.deleteCacheFiles(wayPoint,prof.dataDir,CacheFiles);
-					}
-				}
-			}
 		}
 		pbf.exit(0);
 	}
