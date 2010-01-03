476e20857b14655944779ccf3dfdefa0e061d55b
Arbor95 <arbor95@gmx.de>
2010-01-03 18:12:07 +0000

    additional line for creating ppc w2003 on linux

--- buildexe.sh
+++ buildexe.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 programs/ewecl programs/Jewel.ewe -c cw-pda.jnf
+programs/ewecl programs/Jewel.ewe -c cw-ppc2003.jnf
 programs/ewecl programs/Jewel.ewe -c cw-pc.jnf
 # Dont change the order above because the PC version has to overwrite the PDA version of the EWE-file
