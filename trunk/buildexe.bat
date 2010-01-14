e18c34a5c4b7db79ad1a8d38a61809063629e334
mik77 <mik77@55547f66-8dd2-3569-90c8-08ced114d07d>
2010-01-14 01:43:32 +0000

    ups, correct version restored

--- trunk/buildexe.bat
+++ trunk/buildexe.bat
@@ -1,5 +1,6 @@
 cd %~d0%~p0
 call getres.bat
+java -cp lib/ewe.jar Ewe programs/Jewel.ewe -c cw-pda.jnf
 java -cp lib/ewe.jar Ewe programs/Jewel.ewe -c cw-ppc2003.jnf
 java -cp lib/ewe.jar Ewe programs/Jewel.ewe -c cw-pc.jnf
 REM Dont change the order above because the PC version has to overwrite the PDA version of the EWE-file
