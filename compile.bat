c4943f9a51285992ae48e4efcb6dcd268ab8ebfc
pfefferCode <prog@robert-arnold.de>
2010-02-21 13:32:59 +0000

    include new touch moving map in compile.bat (untestet) / thanks to jukker@www.geoclub.de

--- compile.bat
+++ compile.bat
@@ -1,3 +1,3 @@
 if not exist bin\CacheWolf mkdir bin\CacheWolf
-javac -source 1.3 -target 1.1 -classpath ./lib/CompileEwe.zip;./lib/  -d ./bin/ -deprecation ./src/CacheWolf/*.java ./src/CacheWolf/imp/*.java ./src/CacheWolf/navi/*.java ./src/CacheWolf/exp/*.java ./src/CacheWolf/utils/*.java
+javac -source 1.3 -target 1.1 -classpath ./lib/CompileEwe.zip;./lib/  -d ./bin/ -deprecation ./src/CacheWolf/*.java ./src/CacheWolf/imp/*.java ./src/CacheWolf/navi/*.java ./src/CacheWolf/navi/touchControls/*.java ./src/CacheWolf/exp/*.java ./src/CacheWolf/utils/*.java
 pause
