52723bafc13e45d0681f17c025562402b54f6c42
araber95 <araber95@55547f66-8dd2-3569-90c8-08ced114d07d>
2010-07-18 22:08:55 +0000

    now you can nearly do everything with the tpl - exporter : two new params are introduced singleFile and formatModifier. explanation see doc

--- trunk/src/CacheWolf/MainMenu.java
+++ trunk/src/CacheWolf/MainMenu.java
@@ -465,7 +465,7 @@ public class MainMenu extends MenuBar {
 				fc.addMask("*.tpl");
 				fc.setTitle(MyLocale.getMsg(910,"Select Template file"));
 				if(fc.execute() != FormBase.IDCANCEL){
-					TPLExporter tpl = new TPLExporter( pref,profile, fc.getChosenFile().toString());
+					TPLExporter tpl = new TPLExporter( pref, profile, fc.getChosenFile().toString());
 					tpl.doIt();
 				}
 			}
