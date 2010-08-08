b7e22a4123a0309a77eb86b5f0ae6dbdc4b8eac9
araber95 <araber95@55547f66-8dd2-3569-90c8-08ced114d07d>
2010-08-08 11:33:31 +0000

    1. new gpsd version implementation 2. clearing of logfile moved

--- src/CacheWolf/MainForm.java
+++ src/CacheWolf/MainForm.java
@@ -41,10 +41,6 @@ public class MainForm extends Editor {
 
 		pref.debug = dbg;
 		pref.setPathToConfigFile(pathtoprefxml); // in case pathtoprefxml == null the preferences will determine the path itself
-		//We will now read the preferences. This must be done before initializing the log otherwise the locale preference will not be honoured, because
-		//logInit initializes the MyLocale-Singleton.
-		pref.readPrefFile();
-		pref.logInit();
 		doIt();
 	}
 	protected void checkButtons() {
@@ -70,6 +66,7 @@ public class MainForm extends Editor {
 		InfoBox infB = null;
 		try{
 			pref.readPrefFile();
+			pref.logInit();
 			if (MyLocale.initErrors.length() != 0) {
 				new MessageBox("Error", MyLocale.initErrors, FormBase.OKB).execute();
 			}
