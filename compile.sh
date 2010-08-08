79dcfa0a14fe1b6a377d2de9af7c820b8c7d1109
Arbor95 <arbor95@gmx.de>
2010-08-08 13:40:19 +0000

    update for making cw

--- compile.sh
+++ compile.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
-ENCODING=${ENCODING:=windows-1252}
+ENCODING=${ENCODING:-windows-1252}
 
-javac \
-	-source 1.3 \
-	-target 1.1 \
-	-encoding $ENCODING \
-	-cp ./lib/CompileEwe.zip:./lib/  \
-	-d ./bin/ \
-	-deprecation \
-	-nowarn \
-	./src/CacheWolf/*.java \
-	./src/CacheWolf/*/*/*.java \
-	./src/CacheWolf/*/*.java
+do_compile() {
+   javac \
+      -source 1.3 \
+      -target 1.1 \
+      -encoding "${ENCODING}" \
+      -cp ./lib/CompileEwe.zip:./lib/ \
+      -deprecation \
+      -nowarn \
+      "$@"
+}
+
+do_compile \
+   -d ./bin/ \
+   ./src/CacheWolf/*.java \
+   ./src/CacheWolf/*/*/*.java \
+   ./src/CacheWolf/*/*.java
+do_compile \
+   lib/net/*/*.java \
+   lib/org/*/*.java
