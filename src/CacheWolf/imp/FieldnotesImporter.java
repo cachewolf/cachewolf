09c7b3aea3d7328f71af13f6aded3f983d6bc19a
araber95 <araber95@55547f66-8dd2-3569-90c8-08ced114d07d>
2010-09-06 12:18:47 +0000

    correct setting of status with date

--- src/CacheWolf/imp/FieldnotesImporter.java
+++ src/CacheWolf/imp/FieldnotesImporter.java
@@ -8,10 +8,16 @@ import CacheWolf.CacheType;
 import CacheWolf.MyLocale;
 import CacheWolf.Preferences;
 import CacheWolf.Profile;
+import HTML.Tmpl.Util;
+import HTML.Tmpl.Element.Element;
+import ewe.io.BufferedReader;
+import ewe.io.FileNotFoundException;
 import ewe.io.FileReader;
+import ewe.io.IOException;
 import ewe.io.JavaUtf8Codec;
 import ewe.sys.Vm;
 import ewe.sys.Time;
+import ewe.util.EmptyStackException;
 import ewe.util.mString;
 
 public class FieldnotesImporter {
@@ -42,6 +48,64 @@ public class FieldnotesImporter {
 			Vm.showWait(false);
 		}
 	}
+/* TODO Import of files with UNICODE Charset 
+	private void read_file(String filename)
+			throws FileNotFoundException, 
+				IllegalStateException,
+				IOException, 
+				EmptyStackException
+	{
+		BufferedReader br=openFile(filename);
+
+		String line;
+
+		Element e = null;
+		if(elements.empty())
+			e = __template__;
+		else
+			e = (Element)elements.pop();
+
+		max_includes--;
+		while((line=br.readLine()) != null) {
+			Util.debug_print("Line: " + line);
+			e = parseLine(line+"\n", e);
+		}
+		max_includes++;
+
+		br.close();
+		br=null;
+
+	}
+	
+	private BufferedReader openFile(String filename)
+	throws FileNotFoundException{
+		boolean add_path=true;
+		if(!elements.empty() && !search_path_on_include)
+			add_path=false;
+
+		if(filename.startsWith("/"))
+			add_path=false;
+
+		if(this.path == null)
+			add_path=false;
+
+		Util.debug_print("open " + filename);
+		if(!add_path)
+			return new BufferedReader(new FileReader(filename));
+
+		BufferedReader br=null;
+
+		for(int i=0; i<this.path.length; i++) {
+			try {
+				Util.debug_print("trying " + this.path[i] +	"/" + filename);
+				br = new BufferedReader(new FileReader(this.path[i] + "/" + filename));
+				break;
+			} catch (FileNotFoundException fnfe) {}
+		}
+		if(br == null) throw new FileNotFoundException(filename);
+		return br;
+	}
+ */
 	
 	private void parse(String s) {
 		final byte WPPOS=0;
@@ -63,16 +127,17 @@ public class FieldnotesImporter {
 			CacheHolder ch = cacheDB.get(wayPoint);
 			if (ch!=null) {
 				if (l1[LOGTYPPOS].equals(ch.getGCFoundText())) {
-					ch.setCacheStatus(l1[DATEPOS].replace('T',' ').replace('Z', ' '));
+					String stmp=ch.getCacheStatus();
+					ch.setCacheStatus(l1[DATEPOS].replace('T',' ').replace('Z', ' ').trim());
 					ch.setFound(true);
 				} else {
 					String stmp=ch.getCWLogText(l1[LOGTYPPOS]);
 					if(stmp.equals("")) 
-						ch.setCacheStatus(l1[LOGTYPPOS]);
+						ch.setCacheStatus(l1[LOGTYPPOS]); // eingelesener 
 					else ch.setCacheStatus(stmp); // Statustext (ohne Datum/Uhrzeit)
 					ch.setFound(false);
 				}				
-				ch.getCacheDetails(false).setCacheNotes(logText);
+				if (!logText.equals("")) ch.getCacheDetails(false).setCacheNotes(logText);
 				ch.save();
 			}
 		}
