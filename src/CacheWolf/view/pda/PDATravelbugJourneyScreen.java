831d7d561b9ec838fe4dfd812b154ba10631abcb
apreisser <apreisser@55547f66-8dd2-3569-90c8-08ced114d07d>
2011-04-23 12:28:54 +0000

    More PDA-friendly TravelbugScreen implemented. This version is in an early experimental state and and can only be activated by directly editing Your pref.xml (See Preferences.java line 791 how set MobileGui to true)

---
+++ src/CacheWolf/view/pda/PDATravelbugJourneyScreen.java
@@ -0,0 +1,140 @@
+package CacheWolf.view.pda;
+
+import CacheWolf.MyLocale;
+import CacheWolf.TravelbugJourney;
+import CacheWolf.model.TravelBugScreenModel;
+import ewe.graphics.AniImage;
+import ewe.sys.Vm;
+import ewe.ui.CellConstants;
+import ewe.ui.ControlEvent;
+import ewe.ui.Form;
+
+public class PDATravelbugJourneyScreen extends Form {
+	private static final String LINE = "Line";
+
+	private static final String NEXT_PAGE = "NextPage";
+
+	private static final String PREV_PAGE = "PrevPage";
+
+	private static final String MENUE = "Menue";
+
+	PDAListButton[] listButtons;
+
+	TravelBugScreenModel model;
+	// public boolean onlyLogged = false;
+	// /**
+	// * List of all travelbugs in inventory
+	// */
+	// public TravelbugJourneyList myTravelbugJourneys;
+	//
+	// private Vector tbJourneysShowSet;
+	/**
+	 * The index of the first item in the list shown
+	 */
+	private int firstLine;
+
+	private final int linesOnScreen = 7;
+
+	/**
+	 * The six visible entries in the List
+	 */
+
+	public PDATravelbugJourneyScreen(TravelBugScreenModel travelbugModel) {
+		listButtons = new PDAListButton[linesOnScreen];
+		addListener(this);
+		setTitle("TravelBugs");
+
+		model = travelbugModel;
+
+		// backgroundImage = new Image("bug_vga.gif");
+		for (int i = 0; i < model.allTravelbugJourneys.size(); i++) {
+			model.shownTravelbugJourneys.add(model.allTravelbugJourneys.getTBJourney(i));
+		}
+
+		firstLine = 0;
+		for (int i = 0; i < linesOnScreen; i++) {
+			listButtons[i] = new PDAListButton("", LINE + i);
+			addLast(listButtons[i], CellConstants.STRETCH, CellConstants.FILL);
+		}
+		model.createShowSet();
+		setupTBButtons();
+		PDAMenuButton b1 = new PDAMenuButton("<<<", PREV_PAGE);
+		addNext(b1, CellConstants.HSTRETCH, CellConstants.HFILL);
+		b1 = new PDAMenuButton(MyLocale.getMsg(6052, "MENU"), MENUE);
+		b1.anchor = 0;
+		addNext(b1, CellConstants.HSTRETCH, CellConstants.HFILL);
+		b1 = new PDAMenuButton(">>>", NEXT_PAGE);
+		b1.anchor = CellConstants.EAST;
+		addLast(b1, CellConstants.HSTRETCH, CellConstants.HFILL);
+	}
+
+	public void onControlEvent(ControlEvent ev) {
+		if (ev instanceof ControlEvent) {
+			switch (ev.type) {
+			case ControlEvent.PRESSED:
+				if (ev.action.equals(NEXT_PAGE) && model.shownTravelbugJourneys.size() > firstLine + linesOnScreen) {
+					firstLine += linesOnScreen;
+					setupTBButtons();
+				} else if (ev.action.equals(PREV_PAGE) && firstLine > 0) {
+					firstLine -= linesOnScreen;
+					if (firstLine < 0) {
+						firstLine = 0;
+					}
+					setupTBButtons();
+				} else if (ev.action.startsWith(LINE)) {
+					int line = ev.action.charAt(LINE.length()) - '0';
+					TravelbugJourney tbJourney = (TravelbugJourney) model.shownTravelbugJourneys.get(line + firstLine);
+					Form form = new PDATravelbugDetailPanel(tbJourney, this);
+					form.setPreferredSize(800, 600);
+					form.execute();
+					setupTBButtons();
+				} else if (ev.action.equals(MENUE)) {
+					Form form = new PDATravelbugMenuPanel(this);
+					form.setPreferredSize(800, 600);
+					int execute = form.execute();
+					if (execute == 1){
+						exit(0);
+					}
+					setupTBButtons();
+				}
+				break;
+			default:
+				super.onControlEvent(ev);
+			}
+		}
+	}
+
+	public void setupTBButtons() {
+		for (int i = 0; i < linesOnScreen; i++) {
+			if (i + firstLine < model.shownTravelbugJourneys.size()) {
+				TravelbugJourney tbJourney = (TravelbugJourney) model.shownTravelbugJourneys.get(i + firstLine);
+				String tbName = tbJourney.getTb().getName();
+				listButtons[i].text = tbName;
+				listButtons[i].fromText = tbJourney.getFromWaypoint() + '/' + tbJourney.getFromProfile();
+				listButtons[i].fromLogged = tbJourney.getFromLogged();
+				listButtons[i].toText = tbJourney.getToWaypoint() + '/' + tbJourney.getToProfile();
+				listButtons[i].toLogged = tbJourney.getToLogged();
+				listButtons[i].image = new AniImage("bug_vga.gif");
+			} else {
+				listButtons[i].text =
+						listButtons[i].fromText =
+								listButtons[i].toText = "";
+				listButtons[i].toLogged =
+						listButtons[i].fromLogged = true;
+				listButtons[i].image = null;
+			}
+			listButtons[i].repaint();
+		}
+	}
+
+	public void toggleOnlyLogged() {
+		model.toggleOnlyLogged();
+		firstLine = 0;
+		setupTBButtons();
+	}
+
+	public void createShowSet() {
+		firstLine = 0;
+		model.createShowSet();
+	}
+}
