5744e56bfedc94cadecc2ad780ec83f083e053cf
pfefferCode <prog@robert-arnold.de>
2011-08-07 23:54:01 +0000

    MapLoader: Calculate the distance between tiles more exactly (using the WGS84-ellipsoid instead of assuming a sphere). This is still not exact, because the wg84-ellipsoid is used, but ellipsoid which is used for the projection would be really correct.

--- src/CacheWolf/navi/MapLoader.java
+++ src/CacheWolf/navi/MapLoader.java
@@ -57,7 +57,7 @@ import ewe.util.Vector;
 import ewe.util.mString;
 
 /**
- * 
+ *
  * start offset for language file: 4800
  */
 
@@ -82,7 +82,7 @@ public class MapLoader {
 	boolean fetchOnlyMapWithCache = false;
 
 	/**
-	 * 
+	 *
 	 * @param prxy
 	 * @param prt
 	 * @param wmspath
@@ -146,7 +146,7 @@ public class MapLoader {
 
 	/**
 	 * calculates the Expedia Alti = scale which fits in distance to its edges
-	 * 
+	 *
 	 * @param center
 	 * @param distance
 	 *            in meters
@@ -164,7 +164,7 @@ public class MapLoader {
 	/**
 	 * download maps from expedia at zoomlevel alti and save the maps and the .wfl
 	 * in path
-	 * 
+	 *
 	 * @param center
 	 *            centre of all tiles
 	 * @param radius
@@ -177,7 +177,7 @@ public class MapLoader {
 	 *            in pixels
 	 * @param path
 	 *            without "/" at the end
-	 * 
+	 *
 	 */
 	public void setTiles(CWPoint center, float radius, float scale, Point size, int overlapping) {
 		double metersPerLat = (1000 * (new CWPoint(0, 0)).getDistance(new CWPoint(1, 0)));
@@ -192,8 +192,15 @@ public class MapLoader {
 		// if (toplefti.latDec <= bottomrighti.latDec || toplefti.lonDec >= toplefti.lonDec) throw new IllegalArgumentException("topleft must be left and above bottom right");
 		topleft = new CWPoint(toplefti);
 		bottomright = new CWPoint(bottomrighti);
-		double metersPerLat = (1000.0 * (new CWPoint(0, 0)).getDistance(new CWPoint(1, 0)));
-		double metersPerLon = metersPerLat * java.lang.Math.cos((toplefti.latDec + bottomright.latDec) / 2 / 180 * java.lang.Math.PI);
+		// veraltet: Berechnung auf Kugel: double metersPerLat = (1000.0 * (new CWPoint(0, 0)).getDistance(new CWPoint(1, 0)));
+		// veraltet: Berechnung auf Kugel: double metersPerLon = metersPerLat * java.lang.Math.cos((toplefti.latDec + bottomright.latDec) / 2 / 180 * java.lang.Math.PI);
+
+		CWPoint center = new CWPoint((topleft.latDec + bottomright.latDec)/2, (toplefti.lonDec + bottomrighti.lonDec)/2);
+		CWPoint centerleft = new CWPoint((topleft.latDec + bottomright.latDec)/2, toplefti.lonDec);
+		CWPoint centerbottom = new CWPoint(bottomright.latDec, (toplefti.lonDec + bottomrighti.lonDec)/2);
+		double metersPerLat = 1000.0 * center.getDistance(centerbottom)/(center.latDec - centerbottom.latDec);
+		double metersPerLon = 1000.0 * center.getDistance(centerleft  )/(center.lonDec - centerleft.lonDec);
+
 		double metersperpixel = currentOnlineMapService.getMetersPerPixel(scale);
 		double pixelsPerLat = metersPerLat / metersperpixel;
 		double pixelsPerLon = metersPerLon / metersperpixel;
@@ -282,7 +289,7 @@ public class MapLoader {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param center
 	 * @param scale
 	 * @param pixelsize
@@ -568,7 +575,7 @@ class OnlineMapService {
 	 * This method is used in case the online map service provides only certain steps of
 	 * zoomlevels. In this case the scale in meters per pixel must be returned, which
 	 * will be used instead of the wished scale.
-	 * 
+	 *
 	 * @param scale
 	 * @return
 	 */
@@ -582,7 +589,7 @@ class OnlineMapService {
 
 	/**
 	 * Overlaod this to integrate name of layers
-	 * 
+	 *
 	 * @return friendly service name
 	 */
 	public String getName() {
@@ -593,7 +600,7 @@ class OnlineMapService {
 	 * This method is called to get a name of the
 	 * online map service which will be part of the filename
 	 * used for the downloaded image
-	 * 
+	 *
 	 * @return friendly service name
 	 */
 	public String getNameForFileSystem() {
@@ -608,7 +615,7 @@ class OnlineMapService {
 	 * Overload this and return the URL to the map image, don't call super
 	 * Alternatively overload getUrlForBoundingBoxInternal
 	 * You must overload either this method or getUrlForBoundingBox
-	 * 
+	 *
 	 * @param center
 	 * @param scale
 	 * @param pixelsize
@@ -624,7 +631,7 @@ class OnlineMapService {
 	 * This is made protected and named "...Internal" because a lot of services
 	 * don't work correctly when a map is requested, that is not exactly quadratic
 	 * --> alway use getUrlForCenter...
-	 * 
+	 *
 	 * @param surArea
 	 * @param pixelsize
 	 * @return
@@ -636,7 +643,7 @@ class OnlineMapService {
 	/**
 	 * overload this if your map service uses a special projection
 	 * an return an Area that is quadratic in that projection
-	 * 
+	 *
 	 * @param center
 	 * @param scale
 	 * @param pixelsize
@@ -656,7 +663,7 @@ class OnlineMapService {
 
 	/**
 	 * Overload this (don't call super()) or alternatively overload getMapInfoObjectInternal
-	 * 
+	 *
 	 * @param center
 	 * @param scale
 	 * @param pixelsize
@@ -684,7 +691,7 @@ class WebMapService extends OnlineMapService {
 	double maxscaleWMS;
 
 	/**
-	 * 
+	 *
 	 * @param filename
 	 *            without file extension
 	 * @throws IOException
@@ -782,7 +789,7 @@ class WebMapService extends OnlineMapService {
 	private static final int BOTTOMLEFT_INDEX = 3;
 
 	/**
-	 * 
+	 *
 	 * @param maparea
 	 * @return [0] = topleft, [1] = bottomright, [2] = topright, [3] = bottomleft
 	 */
@@ -875,7 +882,7 @@ class WebMapService extends OnlineMapService {
 	 * a) if only one is in the array 0 is returned
 	 * b) if there are more, find out which one matches the correct zone (e.g. Gauß-Küger stripe)
 	 * Call this routine with center of the area (use Area.getcenter())
-	 * 
+	 *
 	 * @param p
 	 *            Point for which the epsg code is searched for
 	 * @return
