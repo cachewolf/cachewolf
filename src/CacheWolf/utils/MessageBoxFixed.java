ef9a33b93b55109c32b8fbe5bd8257669ee41199
araber95 <araber95@gmail.com>
2011-11-10 10:12:51 +0000

    1. removed berlios from gpl 2. log export to gpx/pq works again

--- src/CacheWolf/utils/MessageBoxFixed.java
+++ src/CacheWolf/utils/MessageBoxFixed.java
@@ -1,15 +1,11 @@
-    /*
+/*
     GNU General Public License
     CacheWolf is a software for PocketPC, Win and Linux that
     enables paperless caching.
     It supports the sites geocaching.com and opencaching.de
 
     Copyright (C) 2006  CacheWolf development team
-    See http://developer.berlios.de/projects/cachewolf/
-    for more information.
-    Contact: 	bilbowolf@users.berlios.de
-    			kalli@users.berlios.de
-
+	See http://www.cachewolf.de/ for more information.
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; version 2 of the License.
@@ -22,7 +18,7 @@
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-    */
+*/
 package CacheWolf.utils;
 /* $Id$ */
 import ewe.sys.mThread;
