2e1aeac702666b1301fc5c822758376b7c82ba63
araber95@gmail.com <araber95@gmail.com@55547f66-8dd2-3569-90c8-08ced114d07d>
2011-11-10 10:12:51 +0000

    1. removed berlios from gpl 2. log export to gpx/pq works again

--- trunk/src/CacheWolf/utils/CWPartialInputStream.java
+++ trunk/src/CacheWolf/utils/CWPartialInputStream.java
@@ -1,3 +1,24 @@
+/*
+    GNU General Public License
+    CacheWolf is a software for PocketPC, Win and Linux that
+    enables paperless caching.
+    It supports the sites geocaching.com and opencaching.de
+
+    Copyright (C) 2006  CacheWolf development team
+	See http://www.cachewolf.de/ for more information.
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; version 2 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 /*********************************************************************************
  *  Ewe Virtual Machine - Version 1.14, January 24, 2002                         *
  *  Copyright (C) 1999-2002 Michael L Brereton <michael_brereton@ewesoft.com>    *
