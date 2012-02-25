77ef5a687f18c7e7122a7c89b8c31985ab4641d2
ColleIsarco <herthameier@yahoo.com>
2012-02-25 14:54:59 +0000

    Informations about Profile/Cache of TB taken from or put into are now displayed on the buttons of the touchscreen-friendly UI (experimentel)

--- src/CacheWolf/view/pda/PDATravelbugJourneyScreen.java
+++ src/CacheWolf/view/pda/PDATravelbugJourneyScreen.java
@@ -1,37 +1,13 @@
-/*
-    GNU General Public License
-    CacheWolf is a software for PocketPC, Win and Linux that
-    enables paperless caching.
-    It supports the sites geocaching.com and opencaching.de
-
-    Copyright (C) 2006  CacheWolf development team
-	See http://www.cachewolf.de/ for more information.
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; version 2 of the License.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
 package CacheWolf.view.pda;
 
-import CacheWolf.MyLocale;
 import CacheWolf.TravelbugJourney;
 import CacheWolf.model.TravelBugJourneyScreenModel;
 import ewe.graphics.AniImage;
-import ewe.sys.Vm;
-import ewe.ui.CellConstants;
 import ewe.ui.ControlConstants;
 import ewe.ui.ControlEvent;
 import ewe.ui.Form;
 
-public class PDATravelbugJourneyScreen extends Form {
+public class PDATravelbugJourneyScreen extends PDAList {
 	private static final String LINE = "Line";
 
 	private static final String NEXT_PAGE = "NextPage";
@@ -40,14 +16,7 @@ public class PDATravelbugJourneyScreen extends Form {
 
 	private static final String MENUE = "Menue";
 
-	PDAListButton[] listButtons;
-
 	TravelBugJourneyScreenModel model;
-
-	/**
-	 * The index of the first item in the list shown
-	 */
-	private int firstLine;
 
 	private final int linesOnScreen = 7;
 
@@ -56,7 +25,6 @@ public class PDATravelbugJourneyScreen extends Form {
 	 */
 
 	public PDATravelbugJourneyScreen(TravelBugJourneyScreenModel travelbugModel) {
-		listButtons = new PDAListButton[linesOnScreen];
 		addListener(this);
 		setTitle("TravelBugs");
 
@@ -66,22 +34,8 @@ public class PDATravelbugJourneyScreen extends Form {
 		for (int i = 0; i < model.allTravelbugJourneys.size(); i++) {
 			model.shownTravelbugJourneys.add(model.allTravelbugJourneys.getTBJourney(i));
 		}
-
-		firstLine = 0;
-		for (int i = 0; i < linesOnScreen; i++) {
-			listButtons[i] = new PDAListButton("", LINE + i);
-			addLast(listButtons[i], CellConstants.STRETCH, CellConstants.FILL);
-		}
-		model.createShowSet();
+		createShowSet();
 		setupTBButtons();
-		PDAMenuButton b1 = new PDAMenuButton("<<<", PREV_PAGE);
-		addNext(b1, CellConstants.HSTRETCH, CellConstants.HFILL);
-		b1 = new PDAMenuButton(MyLocale.getMsg(6052, "MENU"), MENUE);
-		b1.anchor = 0;
-		addNext(b1, CellConstants.HSTRETCH, CellConstants.HFILL);
-		b1 = new PDAMenuButton(">>>", NEXT_PAGE);
-		b1.anchor = CellConstants.EAST;
-		addLast(b1, CellConstants.HSTRETCH, CellConstants.HFILL);
 	}
 
 	public void onControlEvent(ControlEvent ev) {
@@ -120,7 +74,15 @@ public class PDATravelbugJourneyScreen extends Form {
 		}
 	}
 
+	
+	protected PDAListButton createListButton(int i) {
+		return new PDATravelBugJourneyButton ("", LINE + i);
+	}
+
 	public void setupTBButtons() {
+		if (model == null){
+			return;
+		}
 		for (int i = 0; i < linesOnScreen; i++) {
 			if (i + firstLine < model.shownTravelbugJourneys.size()) {
 				TravelbugJourney tbJourney = (TravelbugJourney) model.shownTravelbugJourneys.get(i + firstLine);
