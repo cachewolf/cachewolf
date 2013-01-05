c60c5508fbbea19ed9853a50dafb65d5646fa9bb
araber95 <araber95@gmail.com>
2013-01-05 20:45:42 +0000

    = 1.2 (see there)

--- src/CacheWolf/Log.java
+++ src/CacheWolf/Log.java
@@ -1,29 +1,29 @@
-    /*
-    GNU General Public License
-    CacheWolf is a software for PocketPC, Win and Linux that
-    enables paperless caching.
-    It supports the sites geocaching.com and opencaching.de
+/*
+GNU General Public License
+CacheWolf is a software for PocketPC, Win and Linux that
+enables paperless caching.
+It supports the sites geocaching.com and opencaching.de
 
-    Copyright (C) 2006  CacheWolf development team
+Copyright (C) 2006  CacheWolf development team
 See http://www.cachewolf.de/ for more information.
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; version 2 of the License.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-    */
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 package CacheWolf;
 
 public class Log {
-	private static String MAXLOGICON="MAXLOG";
-	private static String INVALIDLOGICON=null;
+	private static String MAXLOGICON = "MAXLOG";
+	private static String INVALIDLOGICON = null;
 	/** The icon which describes the log e.g. icon_sad */
 	private String icon;
 	/** The date in format yyyy-mm-dd */
@@ -35,36 +35,47 @@ public class Log {
 	/** true, if the logger recommended the cache */
 	private boolean recommended = false;
 
-	/** Create a log from a single line in format<br>
-	 * <pre>RECOMMENDED="1"<img src='ICON'>&nbsp;DATE LOGGER<br>MESSAGE
-	 * or <img src='ICON'>&nbsp;DATE by LOGGER<br>MESSAGE</pre>
+	/**
+	 * Create a log from a single line in format<br>
+	 * 
+	 * <pre>
+	 * RECOMMENDED=&quot;1&quot;&lt;img src='ICON'&gt; DATE LOGGER
+	 * <br>
+	 * MESSAGE
+	 * or &lt;img src='ICON'&gt; DATE by LOGGER
+	 * <br>
+	 * MESSAGE
+	 * </pre>
+	 * 
 	 * @param logLine
 	 */
 	public Log(String logLine) {
-//		RECOMMENDED="1"<img src='icon_smile.gif'>&nbsp;2007-01-14 xyz<br>a wonderful log
+		// RECOMMENDED="1"<img src='icon_smile.gif'>&nbsp;2007-01-14 xyz<br>a wonderful log
 		try {
-			int ic1=logLine.indexOf("RECOMMENDED=\"1\"");
+			int ic1 = logLine.indexOf("RECOMMENDED=\"1\"");
 			if (ic1 >= 0)
-				recommended = true; else recommended = false;
-			ic1=logLine.indexOf("<img src='");
-			int ic2=logLine.indexOf("'",ic1+10);
-			icon=logLine.substring(ic1+10,ic2);
-			int d1=logLine.indexOf(";");
-			date=logLine.substring(d1+1,d1+11);
-			int l1=d1+12;
-			if (logLine.substring(l1,l1+3).equals("by ")) l1+=3;
-			int l2=logLine.indexOf("<br>",l1);
-			logger=logLine.substring(l1,l2);
-			message=logLine.substring(l2+4, logLine.indexOf("]]>", l1));
+				recommended = true;
+			else
+				recommended = false;
+			ic1 = logLine.indexOf("<img src='");
+			int ic2 = logLine.indexOf("'", ic1 + 10);
+			icon = logLine.substring(ic1 + 10, ic2);
+			int d1 = logLine.indexOf(";");
+			date = logLine.substring(d1 + 1, d1 + 11);
+			int l1 = d1 + 12;
+			if (logLine.substring(l1, l1 + 3).equals("by ")) l1 += 3;
+			int l2 = logLine.indexOf("<br>", l1);
+			logger = logLine.substring(l1, l2);
+			message = logLine.substring(l2 + 4, logLine.indexOf("]]>", l1));
 		} catch (Exception ex) {
-			if (logLine.indexOf("<img")<0) { // Have we reached the line that states max logs reached
-				icon=MAXLOGICON;
+			if (logLine.indexOf("<img") < 0) { // Have we reached the line that states max logs reached
+				icon = MAXLOGICON;
 			} else {
-				Global.getPref().log("Error parsing log: "+logLine,ex);
-				icon=INVALIDLOGICON;
+				Global.getPref().log("Error parsing log: " + logLine, ex);
+				icon = INVALIDLOGICON;
 			}
-			date="1900-00-00";
-			logger=message="";
+			date = "1900-00-00";
+			logger = message = "";
 		}
 	}
 
@@ -73,10 +84,10 @@ public class Log {
 	}
 
 	public Log(String icon, String date, String logger, String message, boolean recommended_) {
-		this.icon=icon;
-		this.date=date;
-		this.logger=logger;
-		this.message=stripControlChars(message.trim());
+		this.icon = icon;
+		this.date = date;
+		this.logger = logger;
+		this.message = stripControlChars(message.trim());
 		this.recommended = recommended_;
 	}
 
@@ -84,37 +95,43 @@ public class Log {
 		StringBuffer sb = new StringBuffer(desc.length());
 		for (int i = 0; i < desc.length(); i++) {
 			char c = desc.charAt(i);
-			if (c >= ' ' || c == 10 || c == 13)
-				sb.append(c);
+			if (c >= ' ' || c == 10 || c == 13) sb.append(c);
 		}
 		return sb.toString();
 	}
 
 	public static Log maxLog() {
-		return new Log(MAXLOGICON,"1900-00-00","","");
+		return new Log(MAXLOGICON, "1900-00-00", "", "");
 	}
 
 	public String getIcon() {
 		return icon;
 	}
+
 	public void setIcon(String icon) {
 		this.icon = icon;
 	}
+
 	public String getDate() {
 		return date;
 	}
+
 	public void setDate(String date) {
 		this.date = date;
 	}
+
 	public String getLogger() {
 		return logger;
 	}
+
 	public void setLogger(String logger) {
 		this.logger = logger;
 	}
+
 	public String getMessage() {
 		return message;
 	}
+
 	public void setMessage(String message) {
 		this.message = message.trim();
 	}
@@ -128,11 +145,10 @@ public class Log {
 	}
 
 	// if you change any of these make sure to check image2TypeText in the GPX exporters
-	public static String typeText2Image(String typeText){
-		if (typeText.equals("Found it")||typeText.equals("Found")||typeText.equals("find")) return "icon_smile.gif";
-		if (typeText.equals("Didn't find it")||typeText.equals("Not Found")||typeText.equals("no_find")) return "icon_sad.gif";
-		if (typeText.equals("Write note")||typeText.equals("Note")||typeText.equals("note")
-			||typeText.equals("Not Attempted")||typeText.equals("Other")) return "icon_note.gif";
+	public static String typeText2Image(String typeText) {
+		if (typeText.equals("Found it") || typeText.equals("Found") || typeText.equals("find")) return "icon_smile.gif";
+		if (typeText.equals("Didn't find it") || typeText.equals("Not Found") || typeText.equals("no_find")) return "icon_sad.gif";
+		if (typeText.equals("Write note") || typeText.equals("Note") || typeText.equals("note") || typeText.equals("Not Attempted") || typeText.equals("Other")) return "icon_note.gif";
 		if (typeText.equals("Enable Listing")) return "icon_enabled.gif";
 		if (typeText.equals("Temporarily Disable Listing")) return "icon_disabled.gif";
 		if (typeText.equals("Webcam Photo Taken")) return "icon_camera.gif";
@@ -147,7 +163,9 @@ public class Log {
 		if (typeText.equals("Needs Archived")) return "icon_remove.gif";
 		if (typeText.equals("Update Coordinates")) return "coord_update.gif";
 		if (typeText.equals("Retract Listing")) return "img_redlight.gif";
-		Global.getPref().log("GPX Import: warning, unknown logtype "+typeText+" assuming Write note",null);
+		if (typeText.equals("Announcement")) return "74.png";
+		if (typeText.equals("Unarchived")) return "12.png";
+		Global.getPref().log("GPX Import: warning, unknown logtype " + typeText + " assuming Write note", null);
 		return "icon_note.gif";
 	}
 
@@ -157,11 +175,10 @@ public class Log {
 	}
 
 	/** Return XML representation of log for storing in cache.xml */
-	public String toXML(){
-		StringBuffer s=new StringBuffer(400);
+	public String toXML() {
+		StringBuffer s = new StringBuffer(400);
 		s.append("<LOG>");
-		if (recommended)
-			s.append("RECOMMENDED=\"1\"");
+		if (recommended) s.append("RECOMMENDED=\"1\"");
 		s.append("<![CDATA[");
 		s.append(toHtml());
 		s.append("]]>");
@@ -170,11 +187,11 @@ public class Log {
 	}
 
 	/** Return HTML representation of log for display on screen */
-	public String toHtml(){
-//		<img src='icon_smile.gif'>&nbsp;2007-01-14 xyz<br>a wonderful log
-		if (icon.equals(MAXLOGICON)) return "<hr>"+MyLocale.getMsg(736,"Too many logs")+"<hr>";
-		StringBuffer s=new StringBuffer(300);
-		s.append("<img src='"+icon+"'>");
+	public String toHtml() {
+		// <img src='icon_smile.gif'>&nbsp;2007-01-14 xyz<br>a wonderful log
+		if (icon.equals(MAXLOGICON)) return "<hr>" + MyLocale.getMsg(736, "Too many logs") + "<hr>";
+		StringBuffer s = new StringBuffer(300);
+		s.append("<img src='" + icon + "'>");
 		if (recommended) s.append("<img src='recommendedlog.gif'>");
 		s.append("&nbsp;");
 		s.append(date);
--- src/CacheWolf/Version.java
+++ src/CacheWolf/Version.java
@@ -1,24 +1,24 @@
-    /*
-    GNU General Public License
-    CacheWolf is a software for PocketPC, Win and Linux that
-    enables paperless caching.
-    It supports the sites geocaching.com and opencaching.de
+/*
+GNU General Public License
+CacheWolf is a software for PocketPC, Win and Linux that
+enables paperless caching.
+It supports the sites geocaching.com and opencaching.de
 
-    Copyright (C) 2006  CacheWolf development team
+Copyright (C) 2006  CacheWolf development team
 See http://www.cachewolf.de/ for more information.
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; version 2 of the License.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-    */
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 package CacheWolf;
 
 import com.stevesoft.ewe_pat.Regex;
@@ -30,30 +30,30 @@ import ewe.sys.Vm;
 import ewe.util.Properties;
 
 /**
- * Class to hold and handle version information of the cachewolf project
- * language localisation string at at number 7000
+ * Class to hold and handle version information of the cachewolf project language localisation string at at number 7000
  */
 
 public class Version {
 	static final int VER_MAJOR = 1;
-	static final int VER_MINOR = 3;
-	static final String VER_SVN ="$LastChangedRevision$"; // the number is automatically replaced by subversion to the latest versionnumer of this file (svn:keywords LastChangedRevision)
-	static final int SVN_REVISION = Common.parseInt(VER_SVN.substring(VER_SVN.indexOf(" ")+1, VER_SVN.lastIndexOf(" ")));
+	static final int VER_MINOR = 2;
+	static final String VER_SVN = "$LastChangedRevision$"; // the number is automatically replaced by subversion to the latest versionnumer of this file (svn:keywords LastChangedRevision)
+	static final int SVN_REVISION = Common.parseInt(VER_SVN.substring(VER_SVN.indexOf(" ") + 1, VER_SVN.lastIndexOf(" ")));
 	static final int VERSION_TYPE = 3;
+
 	public static final String VERSION_TYPES(int i) {
-		if (i==0) return MyLocale.getMsg(7000,"Release");
-		if (i==1) return MyLocale.getMsg(7001,"Release candidate");
-		if (i==2) return MyLocale.getMsg(7002,"in development, stable");
-		return MyLocale.getMsg(7003,"in development, newest");
+		if (i == 0) return MyLocale.getMsg(7000, "Release");
+		if (i == 1) return MyLocale.getMsg(7001, "Release candidate");
+		if (i == 2) return MyLocale.getMsg(7002, "in development, stable");
+		return MyLocale.getMsg(7003, "in development, newest");
 	};
 
 	/** only valid after calling checkForUpdates() */
-	static int[] updateavailabe = {0,0,0,0,0};
+	static int[] updateavailabe = { 0, 0, 0, 0, 0 };
 	/** only valid after calling checkForUpdates() */
 	static String[] versionnumbers;
 
 	public static String getRelease() {
-		return Convert.toString(VER_MAJOR) + "." + Convert.toString(VER_MINOR)+ "." + Convert.toString(SVN_REVISION);
+		return Convert.toString(VER_MAJOR) + "." + Convert.toString(VER_MINOR) + "." + Convert.toString(SVN_REVISION);
 	}
 
 	public static String getReleaseDetailed() {
@@ -64,37 +64,39 @@ public class Version {
 
 	/**
 	 * Checks if newer versions of cachewolf are available
+	 * 
 	 * @return [0] = recommended version type, [1]...[4]: 0: no update available, 1: newer version available, 2: version doesn't exists, 3: error
 	 * @throws IOException
 	 */
 	public static void checkForUpdates() throws IOException {
-		String currentVersionsUrl="http://svn.berlios.de/svnroot/repos/cachewolf/trunk/currentversions.txt";
+		String currentVersionsUrl = "http://cachewolf.googlecode.com/svn/trunk/currentversions.txt";
 		Properties curvers = new Properties();
 		curvers.load(new ByteArrayInputStream(UrlFetcher.fetchData(currentVersionsUrl)));
 		versionnumbers = new String[updateavailabe.length];
-		for (int i = updateavailabe.length-1; i >=1; i--) {
-			updateavailabe[i] = checkVersion(curvers, "T"+(i-1), i); // this also sets versionnumber[i]
+		for (int i = updateavailabe.length - 1; i >= 1; i--) {
+			updateavailabe[i] = checkVersion(curvers, "T" + (i - 1), i); // this also sets versionnumber[i]
 		}
 		updateavailabe[0] = Convert.toInt(curvers.getProperty("RecommendedType", "0"));
 	}
 
 	/**
 	 * you must call checkForUpdates() before this method
+	 * 
 	 * @return
 	 */
 	public static String newVersionsArrayToString() {
 		StringBuffer ret = new StringBuffer(500);
-		ret.append(MyLocale.getMsg(7022, "Version type")+"\n");
-		ret.append(VERSION_TYPES(VERSION_TYPE)+getRelease());
-		ret.append(" -> "+MyLocale.getMsg(7010 + updateavailabe[VERSION_TYPE+1], ""+updateavailabe[VERSION_TYPE+1]));
+		ret.append(MyLocale.getMsg(7022, "Version type") + "\n");
+		ret.append(VERSION_TYPES(VERSION_TYPE) + getRelease());
+		ret.append(" -> " + MyLocale.getMsg(7010 + updateavailabe[VERSION_TYPE + 1], "" + updateavailabe[VERSION_TYPE + 1]));
 		ret.append("\n\n");
 
-		for (int i=1; i <= updateavailabe.length -1; i++) {
-			if (updateavailabe[i] != 2 || i-1 == VERSION_TYPE) {
-				ret.append(VERSION_TYPES(i-1));
+		for (int i = 1; i <= updateavailabe.length - 1; i++) {
+			if (updateavailabe[i] != 2 || i - 1 == VERSION_TYPE) {
+				ret.append(VERSION_TYPES(i - 1));
 				if (versionnumbers[i] != null) ret.append(" ").append(versionnumbers[i]).append(" ");
 				if (i == updateavailabe[0]) ret.append("*");
-				if (i-1 == VERSION_TYPE ) ret.append("+");
+				if (i - 1 == VERSION_TYPE) ret.append("+");
 				ret.append("\n");
 			}
 		}
@@ -109,33 +111,39 @@ public class Version {
 			checkForUpdates();
 			return newVersionsArrayToString();
 		} catch (IOException e) {
-			return MyLocale.getMsg(7023, "Error getting current version information") +"\n" + e.getMessage();
-		}
-		finally {
+			return MyLocale.getMsg(7023, "Error getting current version information") + "\n" + e.getMessage();
+		} finally {
 			Vm.showWait(false);
 		}
 	}
 
-	
 	/**
-	 * @param url
+	 * @param curvers
+	 * @param prefix
+	 * @param t
 	 * @return: 1 = newer Version available, 0 = this is up to date, 3 = check failed
 	 */
 	private static int checkVersion(Properties curvers, String prefix, int t) {
 		try {
 			int curvmaj = Convert.toInt(curvers.getProperty(prefix + "VersionMajor", "0"));
 			int curvmin = Convert.toInt(curvers.getProperty(prefix + "VersionMinor", "0"));
-			String svnRString = curvers.getProperty(prefix + "SvnRevision","0");
+			String svnRString = curvers.getProperty(prefix + "SvnRevision", "0");
 			if (svnRString.startsWith("http")) {
 				String tmp;
 				Regex s;
 				int i = svnRString.indexOf(' ');
 				if (i > 0) {
 					tmp = UrlFetcher.fetch(svnRString.substring(0, i));
-					s = new Regex (svnRString.substring(i+1, svnRString.length())); // flyingfish works 3/2008 with ("(?i)Revision[\\s]*[:=][\\s]*[\\\\r]*[\\\\n]*[\\s]*([0-9]*)");
-				} else { versionnumbers[t] = "error: no RegEx"; return 3; }
+					s = new Regex(svnRString.substring(i + 1, svnRString.length())); // flyingfish works 3/2008 with ("(?i)Revision[\\s]*[:=][\\s]*[\\\\r]*[\\\\n]*[\\s]*([0-9]*)");
+				} else {
+					versionnumbers[t] = "error: no RegEx";
+					return 3;
+				}
 				s.search(tmp);
-				if (!s.didMatch()) { versionnumbers[t] = "error: RegEx didnot match"; return 3; }
+				if (!s.didMatch()) {
+					versionnumbers[t] = "error: RegEx didnot match";
+					return 3;
+				}
 				svnRString = s.stringMatched(1);
 			}
 			versionnumbers[t] = curvmaj + "." + curvmin + "." + svnRString;
