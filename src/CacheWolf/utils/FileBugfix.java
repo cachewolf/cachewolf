73178207b1c2d61719e3737b25eb4b5be7e608ac
araber95 <araber95@gmail.com>
2013-12-21 19:36:38 +0000

    missing(no) dirs in list of File (MapsList) in exe-Version: use FileBugfix now

--- src/CacheWolf/utils/FileBugfix.java
+++ src/CacheWolf/utils/FileBugfix.java
@@ -33,127 +33,136 @@ import ewe.util.mString;
  *         so I get all the files which null in spite of the mask and filter afterwords
  */
 public class FileBugfix extends File {
-	File ewefile;
+    File ewefile;
 
-	public FileBugfix(String path) {
-		super(STRreplace.replace(path, "//", "/"));
-		ewefile = ewe.sys.Vm.newFileObject();
-		ewefile.set(null, name);
-	}
+    public FileBugfix(String path) {
+	super(STRreplace.replace(path, "//", "/"));
+	ewefile = ewe.sys.Vm.newFileObject();
+	ewefile.set(null, name);
+    }
 
-	public FileBugfix(String path, String fName) {
-		this(path + "/" + fName);
-	}
+    public FileBugfix(String path, String fName) {
+	this(path + "/" + fName);
+    }
 
-	public void set(File dir, String name) {
-		ewefile.set(dir, STRreplace.replace(name, "//", "/")); // this or null?
-	}
+    // Overrides: set(...) in File
+    public void set(File dir, String name) {
+	ewefile.set(dir, STRreplace.replace(name, "//", "/")); // this or null?
+    }
 
-	public String[] list(final String mask, final int listAndSortOptions) {
-		return listBugFixed(mask, listAndSortOptions);
-		/*
-		 * super.list has on some systems (linux, but also Windows Mobile 2003) a bug 
-		 * in ewe 1.49: 
-		 * when filenames contain more than one ".", 
-		 * then the mask matches on these systems the first in spite of the last "."
-		 * listmultiple doesn't have this bug 
-		 */
-	}
-
-	public String[] listMultiple(final String compositeMask, final int listAndSortOptions) {
-		/* super.listMultiple in ewe 1.49
-		 * usually works correct, but when called with Option Dirs_Only, it gives the dirs twice (once filtered by mask, once all)
-		 */
-		return listBugFixed(compositeMask, listAndSortOptions);
-	}
-
-	private String[] listBugFixed(final String compositeMask, final int listAndSortOptions) {
-		String mask = (compositeMask == null) ? "*.*" : compositeMask;
-		String[] found; //the following code is mainly copied from FileBase.listmultiple to avoid recursion it is not called
-		char c = mask.indexOf(',') == -1 ? ';' : ',';
-		String masks[] = mString.split(mask, c);
-		String dirs[] = new String[0];
-		if ((listAndSortOptions & LIST_FILES_ONLY) == 0)
-			dirs = ewefile.list(null, LIST_DIRECTORIES_ONLY); // add dirs if not only asked for files
-		if ((listAndSortOptions & LIST_DIRECTORIES_ONLY) == 0)
-			found = ewefile.list(null, FileBase.LIST_FILES_ONLY | listAndSortOptions); // add files if not dirs only
-		else {
-			found = dirs; // if dirs only -> apply masks to the dirs
-			dirs = new String[0]; // this line is missing in ewe FileBase.listmultiple -> doubled dirs when using listmultiple with the option dirs_only
-		}
-		if (found == null)
-			return null;
-		ewe.util.FileComparer[] fcs = new ewe.util.FileComparer[masks.length];
-
-		for (int i = 0; i < masks.length; i++)
-			fcs[i] = new FileComparer(ewefile, ewe.sys.Vm.getLocale(), listAndSortOptions, masks[i]);
-		int left = found.length;
-		for (int i = 0; i < found.length; i++) {
-			boolean matched = false;
-			for (int f = 0; f < fcs.length; f++) {
-				if (fcs[f].matches(found[i])) {
-					matched = true;
-					break;
-				}
-			}
-			if (!matched) {
-				found[i] = null;
-				left--;
-			}
-		}
-		String[] isMatching = new String[dirs.length + left];
-		ewe.sys.Vm.copyArray(dirs, 0, isMatching, 0, dirs.length);
-		for (int i = 0; i < dirs.length; i++)
-			isMatching[i] = isMatching[i].replace('\\', '/'); // on some PDAs a "\" in the path seems to make problems, but it seems that is ewe (files.list) returns sometimes a path containing a "\"
-		for (int i = 0, d = dirs.length; i < found.length; i++)
-			if (found[i] != null)
-				isMatching[d++] = found[i].replace('\\', '/'); // on some PDAs a "\" in the path seems to make problems, but it seems that is ewe (files.list) returns sometimes a path containing a "\"
-		found = isMatching;
-		return found;
-	}
-
-	/**
-	 * this is needed in order to be able to use the simulated file system _filesystem.zip when running as applet
+    // Overrides: list(...) in FileBase
+    public String[] list(final String mask, final int listAndSortOptions) {
+	return listBugFixed(mask, listAndSortOptions);
+	/*
+	 * super.list has on some systems (linux, but also Windows Mobile 2003) a bug 
+	 * in ewe 1.49: 
+	 * when filenames contain more than one ".", 
+	 * then the mask matches on these systems the first in spite of the last "."
+	 * listmultiple doesn't have this bug 
 	 */
+    }
 
-	public boolean exists() {
-		return (ewefile.exists());
+    // Overrides: listMultiple(...) in FileBase
+    public String[] listMultiple(final String compositeMask, final int listAndSortOptions) {
+	/* super.listMultiple in ewe 1.49
+	 * usually works correct, but when called with Option Dirs_Only, it gives the dirs twice (once filtered by mask, once all)
+	 */
+	return listBugFixed(compositeMask, listAndSortOptions);
+    }
+
+    private String[] listBugFixed(final String compositeMask, final int listAndSortOptions) {
+	String mask = (compositeMask == null) ? "*.*" : compositeMask;
+	String[] found; //the following code is mainly copied from FileBase.listmultiple to avoid recursion it is not called
+	char c = mask.indexOf(',') == -1 ? ';' : ',';
+	String masks[] = mString.split(mask, c);
+	String dirs[] = new String[0];
+	if ((listAndSortOptions & LIST_FILES_ONLY) == 0)
+	    dirs = ewefile.list(null, LIST_DIRECTORIES_ONLY); // add dirs if not only asked for files
+	if ((listAndSortOptions & LIST_DIRECTORIES_ONLY) == 0)
+	    found = ewefile.list(null, FileBase.LIST_FILES_ONLY | listAndSortOptions); // add files if not dirs only
+	else {
+	    found = dirs; // if dirs only -> apply masks to the dirs
+	    dirs = new String[0]; // this line is missing in ewe FileBase.listmultiple -> doubled dirs when using listmultiple with the option dirs_only
 	}
+	if (found == null)
+	    return null;
+	ewe.util.FileComparer[] fcs = new ewe.util.FileComparer[masks.length];
 
-	public boolean isDirectory() {
-		return (ewefile.isDirectory());
-	}
-
-	public boolean createDir() {
-		return (ewefile.createDir());
-
-	}
-
-	public boolean delete() {
-		return (ewefile.delete());
-	}
-
-	public int getLength() {
-		return (ewefile.getLength());
-	}
-
-	public String getDrivePath() {
-		return (ewefile.getDrivePath());
-	}
-
-	public String getAbsolutePath() {
-		String fullPath = ewefile.getFullPath();
-		int rel = fullPath.indexOf("/..");
-		while (rel > 0) {
-			int parent = fullPath.lastIndexOf('/', rel - 1);
-			if (parent == -1) {
-				break;
-			}
-			fullPath = fullPath.substring(0, parent) + fullPath.substring(rel + 3);
-
-			rel = fullPath.indexOf("/..");
+	for (int i = 0; i < masks.length; i++)
+	    fcs[i] = new FileComparer(ewefile, ewe.sys.Vm.getLocale(), listAndSortOptions, masks[i]);
+	int left = found.length;
+	for (int i = 0; i < found.length; i++) {
+	    boolean matched = false;
+	    for (int f = 0; f < fcs.length; f++) {
+		if (fcs[f].matches(found[i])) {
+		    matched = true;
+		    break;
 		}
-		return fullPath;
+	    }
+	    if (!matched) {
+		found[i] = null;
+		left--;
+	    }
 	}
+	String[] isMatching = new String[dirs.length + left];
+	ewe.sys.Vm.copyArray(dirs, 0, isMatching, 0, dirs.length);
+	for (int i = 0; i < dirs.length; i++)
+	    isMatching[i] = isMatching[i].replace('\\', '/'); // on some PDAs a "\" in the path seems to make problems, but it seems that is ewe (files.list) returns sometimes a path containing a "\"
+	for (int i = 0, d = dirs.length; i < found.length; i++)
+	    if (found[i] != null)
+		isMatching[d++] = found[i].replace('\\', '/'); // on some PDAs a "\" in the path seems to make problems, but it seems that is ewe (files.list) returns sometimes a path containing a "\"
+	found = isMatching;
+	return found;
+    }
+
+    /**
+     * this is needed in order to be able to use the simulated file system _filesystem.zip when running as applet
+     */
+    // Overrides: exists() in File
+    public boolean exists() {
+	return (ewefile.exists());
+    }
+
+    //Overrides: isDirectory() in File
+    public boolean isDirectory() {
+	return (ewefile.isDirectory());
+    }
+
+    // Overrides: createDir() in File
+    public boolean createDir() {
+	return (ewefile.createDir());
+
+    }
+
+    // Overrides: delete() in File
+    public boolean delete() {
+	return (ewefile.delete());
+    }
+
+    // Overrides: getLength() in File
+    public int getLength() {
+	return (ewefile.getLength());
+    }
+
+    // Overrides: getDrivePath() in FileBase
+    public String getDrivePath() {
+	return (ewefile.getDrivePath());
+    }
+
+    // Overrides: getAbsolutePath() in FileBase
+    public String getAbsolutePath() {
+	String fullPath = ewefile.getFullPath();
+	int rel = fullPath.indexOf("/..");
+	while (rel > 0) {
+	    int parent = fullPath.lastIndexOf('/', rel - 1);
+	    if (parent == -1) {
+		break;
+	    }
+	    fullPath = fullPath.substring(0, parent) + fullPath.substring(rel + 3);
+
+	    rel = fullPath.indexOf("/..");
+	}
+	return fullPath;
+    }
 
 }
