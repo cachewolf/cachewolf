5fb7159f3b82e85bbe99c3599747a5caf4d64a51
ColleIsarco <herthameier@yahoo.de>
2021-12-04 14:48:13 +0100

    Removed unused class OCLinkImport and its menu-entry.

--- runwolf.sh
+++ runwolf.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd work
-/opt/jdk1.8.0_144/bin/java -cp ../programs/ewe.jar:../lib:../bin Ewe CacheWolf.CacheWolf
+java -cp ../programs/ewe.jar:../lib:../bin Ewe CacheWolf.CacheWolf
 cd ..
